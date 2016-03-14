7ca618d52fad669024f91997e7dc340341c5f24d
Justin Coyne <jcoyne@justincoyne.com>
2016-03-14 12:42:59 -0500

    Permit ordered_member_ids so that the file manager works. Fixes #66

--- spec/forms/curation_concerns/section_form_spec.rb
+++ spec/forms/curation_concerns/section_form_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe CurationConcerns::SectionForm do
   describe "#terms" do
     subject { described_class.terms }
-    it { is_expected.to eq [:title, :monograph_id] }
+    it { is_expected.to eq [:title, :monograph_id, :ordered_member_ids] }
   end
 
   describe "#required_fields" do
