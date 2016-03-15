3502f21e00f24f7d3e692d1d61b8d41d9559e1ce
Justin Coyne <jcoyne@justincoyne.com>
2016-03-15 11:46:33 -0500

    For now, logged in users are admins

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,23 @@
 class Ability
   include Hydra::Ability
-
   include CurationConcerns::Ability
-  self.ability_logic += [:everyone_can_create_curation_concerns]
 
   # Define any customized permissions here.
   def custom_permissions
-    # Limits deleting objects to a the admin user
-    #
-    # if current_user.admin?
-    #   can [:destroy], ActiveFedora::Base
-    # end
+    if admin?
+      can [:destroy], ActiveFedora::Base
+    end
 
     # Limits creating new objects to a specific group
     #
     # if user_groups.include? 'special_group'
     #   can [:create], ActiveFedora::Base
     # end
+  end
+
+  # TODO: For now, any signed in user is an admin
+  def admin?
+    # user_groups.include? 'admin'
+    registered_user?
   end
 end
