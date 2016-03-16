f40ac1714d88acfbadee0b1bf731d6688d3ba975
Justin Coyne <jcoyne@justincoyne.com>
2016-03-16 16:01:20 -0500

    Configure to run jobs in resque

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Heliotrope
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_job.queue_adapter = :resque
   end
 end
