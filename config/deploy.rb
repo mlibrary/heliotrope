86f0c9408caa1fe140b142f12c01a363bd5916f4
Alicia Cozine <alicia@curationexperts.com>
2016-03-16 09:54:57 -0500

    shares log dir, closes #74

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,10 @@ set :linked_files,
     )
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs,
+    %w(
+      log
+    )
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
