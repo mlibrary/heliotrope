21676b0f7458815d3ff19a027f3bb02bc90a3741
Conor O'Malley <conorom@umich.edu>
2016-04-26 10:16:35 -0400

    Change text on 'Organize Assets' button, add different versions for sections v. monographs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def manage_assets_btn_text
+    if @presenter.human_readable_type == 'Monograph'
+      t("manage_assets.monograph_link_text")
+    else
+      t("manage_assets.section_link_text")
+    end
+  end
 end
