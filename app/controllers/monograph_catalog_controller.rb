0c57b057c606d144af6e837f5d905b37c5bb23db
Seth A. Johnson <seth.adams.johnson@gmail.com>
2016-06-30 11:33:13 -0400

    fix for facet limit bug #278

--- app/controllers/monograph_catalog_controller.rb
+++ app/controllers/monograph_catalog_controller.rb
@@ -4,11 +4,19 @@ class MonographCatalogController < ::CatalogController
   configure_blacklight do |config|
     config.search_builder_class = MonographSearchBuilder
 
-    config.add_facet_field solr_name('keywords', :facetable), label: "Keyword"
+    config.add_facet_field solr_name('keywords', :facetable), label: "Keyword", limit: 5
     config.add_facet_field solr_name('section_title', :facetable), label: "Section", limit: 5
     config.add_facet_fields_to_solr_request!
 
     config.index.partials = [:index_header, :thumbnail, :index]
+  end
+
+  # Override blacklight, controllers/concerns/blacklight/controller.rb
+  # For bug #278. TODO: This is probably wrong and should be handled in routes somehow?
+  def search_facet_url(options = {})
+    opts = search_state.to_h.merge(action: "facet").merge(options).except(:page)
+    opts['controller'] = 'catalog'
+    url_for opts
   end
 
   private
