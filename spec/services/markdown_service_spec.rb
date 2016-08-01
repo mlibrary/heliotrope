743234120a34d6cdf52f8389f6f634404e0d306d
Seth A. Johnson <seth.adams.johnson@gmail.com>
2016-08-01 13:19:48 -0400

    added strikethrough markdown support, #326

--- spec/services/markdown_service_spec.rb
+++ spec/services/markdown_service_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe MarkdownService do
-  let(:html) { "This is _italics_ and this is __bold__" }
+  let(:html) { "This is _italics_ and this is __bold__ and this is ~~strikethrough~~" }
 
   it "renders html from markdown" do
-    expect(described_class.markdown(html)).to eq("This is <em>italics</em> and this is <strong>bold</strong>\n")
+    expect(described_class.markdown(html)).to eq("This is <em>italics</em> and this is <strong>bold</strong> and this is <del>strikethrough</del>\n")
   end
 end
