c25f7d8739828fdcfc057e4e65ada4fad44a5f75
Conor O'Malley <conorom@umich.edu>
2016-08-04 15:21:38 -0400

    only remove outermost p tags

--- spec/services/markdown_service_spec.rb
+++ spec/services/markdown_service_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe MarkdownService do
-  let(:html) { "This is _italics_ and this is __bold__ and this is ~~strikethrough~~" }
+  let(:html) { "\n\nThis is _italics_ and this is\n\na paragraph\n\n__bold__ and this is ~~strikethrough~~" }
 
   it "renders html from markdown" do
-    expect(described_class.markdown(html)).to eq("This is <em>italics</em> and this is <strong>bold</strong> and this is <del>strikethrough</del>\n")
+    expect(described_class.markdown(html)).to eq("This is <em>italics</em> and this is</p>\n\n<p>a paragraph</p>\n\n<p><strong>bold</strong> and this is <del>strikethrough</del>")
   end
 end
