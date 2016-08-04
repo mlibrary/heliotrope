faf6f665e48701529dfabb4e034c11080d6e850c
Conor O'Malley <conorom@umich.edu>
2016-08-04 15:21:38 -0400

    only remove outermost p tags

--- app/services/markdown_service.rb
+++ app/services/markdown_service.rb
@@ -3,6 +3,6 @@ class MarkdownService
   self.md = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(safe_links_only: true), strikethrough: true, lax_spacing: true, no_intra_emphasis: true)
 
   def self.markdown(value)
-    md.render(value).gsub(/^<p>/, "").gsub(/<\/p>$/, "")
+    Regexp.new(/\A<p>(.*)<\/p>\Z/m).match(md.render(value))[1] rescue md.render(value)
   end
 end
