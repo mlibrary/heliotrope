6ece6a6351807b95fbb601b7460c7a2171d210c0
Greg Kostin <gkostin@umich.edu>
2016-08-26 13:43:37 -0400

    387 Markdown in monograph description

--- app/assets/javascripts/short_monograph_description.js
+++ app/assets/javascripts/short_monograph_description.js
@@ -3,14 +3,42 @@ jQuery(function(){
     var minimized_elements = $('p.description');
 
     minimized_elements.each(function(){
-        var t = $(this).text();
-        if(t.length < 500) return;
+        // TODO: Threshold should probably be passed in as a parameter.
+        var threshold = 500;
+        if ($(this).text().length <= threshold) return;
 
-        $(this).html(
-            t.slice(0,500)+'<span>... </span><a href="#" class="more">More</a>'+
-            '<span style="display:none;">'+ t.slice(500,t.length)+' <a href="#" class="less">Less</a></span>'
-        );
+        $md = $($.parseHTML($(this).html()));
+        var m = $md.length;
+        var n = 0;
+        var text = "";
+        var total = 0;
+        $(this).html(text);
+        for(n=0; n<m; n++){
+          // Text nodes have a nodeType of 3
+          text = ($md[n].nodeType == 3) ? $md[n].data : $md[n].innerText;
+          total += text.length;
+          if (total > threshold) break;
+          $(this).append($md[n]);
+        }
 
+        var index = text.length - (total - threshold);
+        $clone = $($md[n]).clone();
+        // Text nodes have a nodeType of 3
+        if ($md[n].nodeType == 3) {
+          $clone[0].textContent = text.slice(0,index);
+          $md[n].textContent = text.slice(index,text.length);
+        } else {
+          $clone[0].innerHTML = text.slice(0,index);
+          $md[n].innerHTML = text.slice(index,text.length);
+        }
+        $(this).append($clone[0]);
+        $(this).append('<span>... </span><a href="#" class="more">More</a>');
+        $less = $('<span style="display:none;"></span>');
+        for(; n<m; n++){
+          $less.append($md[n]);
+        }
+        $less.append(' <a href="#" class="less">Less</a>');
+        $(this).append($less);
     });
 
     $('a.more', minimized_elements).click(function(event){
@@ -23,5 +51,4 @@ jQuery(function(){
         event.preventDefault();
         $(this).parent().hide().prev().show().prev().show();
     });
-
 });
