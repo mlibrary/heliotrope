5adf29f09238ce9b309870bf0ea20a7f71a928c1
Greg Kostin <gkostin@umich.edu>
2016-10-17 14:52:27 -0400

    497 Ampersand displayed in page title.

--- spec/services/markdown_service_spec.rb
+++ spec/services/markdown_service_spec.rb
@@ -6,4 +6,26 @@ describe MarkdownService do
   it "renders html from markdown" do
     expect(described_class.markdown(html)).to eq("This is <em>italics</em> and this is</p>\n\n<p>a paragraph</p>\n\n<p><strong>bold</strong> a line<br>\nbreak and this is <del>strikethrough</del>")
   end
+
+  describe '.markdown_as_text' do
+    it 'renders italics' do
+      expect(described_class.markdown_as_text('_italics_')).to eq('italics')
+    end
+
+    it 'renders bold' do
+      expect(described_class.markdown_as_text('__bold__')).to eq('bold')
+    end
+
+    it 'renders strike-through' do
+      expect(described_class.markdown_as_text('~~strike-through~~')).to eq('strike-through')
+    end
+
+    it 'renders line breaks' do
+      expect(described_class.markdown_as_text("a line\nbreak")).to eq('a line break')
+    end
+
+    it 'renders paragraphs' do
+      expect(described_class.markdown_as_text("\n\n\n\n\nfirst paragraph\nsecond paragraph\n\nthird paragraph\n\n\n\n\n")).to eq('first paragraph second paragraph third paragraph')
+    end
+  end
 end
