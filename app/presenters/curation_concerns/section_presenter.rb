7834f48254e57ba3e8dada1329f8fab5be6bf27e
Seth A. Johnson <seth.adams.johnson@gmail.com>
2016-10-18 10:10:23 -0400

    no recursive representative presenter search for sections, see #537

--- app/presenters/curation_concerns/section_presenter.rb
+++ app/presenters/curation_concerns/section_presenter.rb
@@ -4,6 +4,17 @@ module CurationConcerns
       member_of.first.fetch('title_tesim', []).first
     end
 
+    # In CurationConcerns::WorkShowPresenter this method would produce an endless loop.
+    # The representative_id of the Section points to the FileSet.
+    # The FileSet's representative_id is then used to call representative_presenter,
+    # which then uses that same FileSet representative_id to call a representative_presenter, etc, etc.
+    # This override is simplistic but works for our Sections which contain only FileSets,
+    # no recursion needed
+    def representative_presenter
+      return nil if representative_id.blank?
+      @representative_presenter ||= CurationConcerns::PresenterFactory.build_presenters([representative_id], CurationConcerns::FileSetPresenter, current_ability).first
+    end
+
     private
 
       def member_of
--- spec/presenters/curation_concerns/section_presenter_spec.rb
+++ spec/presenters/curation_concerns/section_presenter_spec.rb
@@ -1,31 +1,37 @@
 require 'rails_helper'
 
 describe CurationConcerns::SectionPresenter do
-  let(:solr_document) { SolrDocument.new(attributes) }
-  let(:attributes) do
-    { "id" => "qr46r109z",
-      "title_tesim" => ["foo bar"],
-      "human_readable_type_tesim" => ["Generic Work"],
-      "has_model_ssim" => ["GenericWork"],
-      "monograph_id_ssim" => "dr26xx448" }
-  end
-
   let(:ability) { nil }
-  let(:presenter) { described_class.new(solr_document, ability) }
-
-  let(:response) do
-    [{ "member_ids_ssim" => ["qr46r109z"],
-       "title_tesim" => ["My first monograph"] }]
-  end
-
-  before do
-    allow(ActiveFedora::SolrService).to receive(:query)
-      .with("{!terms f=member_ids_ssim}qr46r109z")
-      .and_return(response)
-  end
 
   describe "#monograph_label" do
-    subject { presenter.monograph_label }
-    it { is_expected.to eq 'My first monograph' }
+    let(:solr_document) { SolrDocument.new(attributes) }
+    let(:attributes) do
+      { "id" => "qr46r109z",
+        "title_tesim" => ["foo bar"],
+        "human_readable_type_tesim" => ["Section"],
+        "has_model_ssim" => ["Section"],
+        "monograph_id_ssim" => "dr26xx448"
+      }
+    end
+    let(:presenter) { described_class.new(solr_document, ability) }
+
+    it "returns the it's monograph's label" do
+      allow(ActiveFedora::SolrService).to receive(:query)
+        .with("{!terms f=member_ids_ssim}qr46r109z")
+        .and_return([{ "member_ids_ssim" => ["qr46r109z"],
+                       "title_tesim" => ["My first monograph"] }])
+
+      expect(presenter.monograph_label).to eq 'My first monograph'
+    end
+  end
+
+  describe "#representative_presenter" do
+    let(:file_set) { create(:file_set) }
+    let(:section) { create(:section, title: ["Section 1"], representative_id: file_set.id) }
+    let(:presenter) { CurationConcerns::PresenterFactory.build_presenters([section.id], described_class, ability).first }
+
+    it "returns a FileSetPresenter" do
+      expect(presenter.representative_presenter.class).to eq CurationConcerns::FileSetPresenter
+    end
   end
 end
