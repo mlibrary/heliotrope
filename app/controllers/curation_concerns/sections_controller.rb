45fd89348c94dae4ec27693f2b628a7f22ffc525
Seth A. Johnson <seth.adams.johnson@gmail.com>
2016-10-25 11:27:59 -0400

    Fixes #542. Changing a Section title should not break the Section facet.
    When a Section title is edited, we need to reindex all of it's file_sets since
    we use the file_sets indexes section_title_tesim to build the Section facet
    on the monograph_catalog page. Added a reindex_file_set_job that gets called
    from by section_controller#update when a section's title(s) has changed.

--- app/controllers/curation_concerns/sections_controller.rb
+++ app/controllers/curation_concerns/sections_controller.rb
@@ -6,4 +6,16 @@ class CurationConcerns::SectionsController < ApplicationController
   def search_builder_class
     ::SectionSearchBuilder
   end
+
+  def update
+    # If a section's title is changed, we need to re-index all of it's file_sets
+    # in order for the section facet on the monograph_catalog page to work right
+    # see #542
+    if curation_concern.title != params[:section][:title]
+      curation_concern.members.each do |member|
+        ReindexFileSetJob.perform_later(member) if member.file_set?
+      end
+    end
+    super
+  end
 end
