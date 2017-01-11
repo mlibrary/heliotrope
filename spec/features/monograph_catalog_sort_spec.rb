80277829aff4d3ffcc7adb472e3ba7163111c58e
val99erie <val99erie@gmail.com>
2017-01-11 15:45:10 -0600

    Minor refactor to a spec for readability

--- spec/features/monograph_catalog_sort_spec.rb
+++ spec/features/monograph_catalog_sort_spec.rb
@@ -10,6 +10,8 @@ feature 'Monograph catalog sort' do
     let(:fileset_outlier) { create(:file_set, title: ['Outlier'], search_year: '2000', resource_type: ['video'], date_uploaded: DateTime.new(2001, 2, 3, 4, 1, 0, '+0')) }
     let(:monograph) { create(:monograph, user: user, title: ['Polka Dots'], representative_id: cover.id) }
     let(:fileset_count) { 24 }
+    let(:down_arrow) { "\u25BC" }
+    let(:up_arrow) { "\u25B2" }
 
     before do
       stub_out_redis
@@ -23,7 +25,6 @@ feature 'Monograph catalog sort' do
       monograph.save!
     end
 
-    # for reference "\u25BC" is desc (down arrow), "\u25B2" is asc (up arrow)
     scenario 'displays 20 results by default and is ordered as expected' do
       visit monograph_catalog_path(id: monograph.id)
       # 20 results on page by default
@@ -35,33 +36,33 @@ feature 'Monograph catalog sort' do
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[fileset_count + 1].title.first
 
       # this control is styled as a drop-down but is actually a button & list, hence click_link not select
-      click_link "Section \u25BC"
+      click_link "Section #{down_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # [0] would be the representative file, not shown on this page...
       # this is the outlier file as it was uploaded second (shows up second last)
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[1].title.first
 
-      click_link "Section \u25B2"
+      click_link "Section #{up_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # essentially default sort, sans score, last uploaded is first
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[fileset_count + 1].title.first
 
-      click_link "Format \u25BC"
+      click_link "Format #{down_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # highest resource_type is the fileset_outlier, second ordered member, set to 'video' above
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[1].title.first
 
-      click_link "Format \u25B2"
+      click_link "Format #{up_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # lowest resource_type is the 3rd ordered member (first using the audio sequence, should be set to 'audio0001')
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[2].title.first
 
-      click_link "Year \u25BC"
+      click_link "Year #{down_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # highest year is the fileset_outlier, second ordered member [0, 1,... ], manually set to '2000' above
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[1].title.first
 
-      click_link "Year \u25B2"
+      click_link "Year #{up_arrow}"
       first_fileset_link_text = page.first('.documentHeader .index_title a').text
       # lowest year is the third ordered member, the first to use the factory's sequence and set to '1900'
       expect(first_fileset_link_text).to eq monograph.ordered_members.to_a[2].title.first
