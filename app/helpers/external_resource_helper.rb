2148d157d9645464bc05e1e05bb42c97d8cd2501
Conor O'Malley <conorom@umich.edu>
2017-04-22 15:01:59 -0400

    resource-type-based icons for external resources

---
+++ app/helpers/external_resource_helper.rb
@@ -0,0 +1,16 @@
+module ExternalResourceHelper
+  def glyphicon_type(resource_type)
+    case resource_type.downcase
+    when 'text'
+      'glyphicon glyphicon-file'
+    when 'image'
+      'glyphicon glyphicon-picture'
+    when 'video'
+      'glyphicon glyphicon-film'
+    when 'audio'
+      'glyphicon glyphicon-volume-up'
+    else
+      'glyphicon glyphicon-file'
+    end
+  end
+end
