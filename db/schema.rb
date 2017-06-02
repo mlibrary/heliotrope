c78d990c474b00e813d15da37f5d6e0acc9ca297
Jonathan McGlone <jmcglone@umich.edu>
2017-06-02 12:59:43 -0400

    add acls heb with light theme branding

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,9 +20,8 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "document_type"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
-
-  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "checksum_audit_logs", force: :cascade do |t|
     t.string   "file_set_id"
@@ -34,9 +32,8 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "actual_result"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["file_set_id", "file_id"], name: "by_file_set_id_and_file_id"
   end
-
-  add_index "checksum_audit_logs", ["file_set_id", "file_id"], name: "by_file_set_id_and_file_id"
 
   create_table "curation_concerns_operations", force: :cascade do |t|
     t.string   "status"
@@ -53,12 +50,11 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.integer  "children_count", default: 0, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.index ["lft"], name: "index_curation_concerns_operations_on_lft"
+    t.index ["parent_id"], name: "index_curation_concerns_operations_on_parent_id"
+    t.index ["rgt"], name: "index_curation_concerns_operations_on_rgt"
+    t.index ["user_id"], name: "index_curation_concerns_operations_on_user_id"
   end
-
-  add_index "curation_concerns_operations", ["lft"], name: "index_curation_concerns_operations_on_lft"
-  add_index "curation_concerns_operations", ["parent_id"], name: "index_curation_concerns_operations_on_parent_id"
-  add_index "curation_concerns_operations", ["rgt"], name: "index_curation_concerns_operations_on_rgt"
-  add_index "curation_concerns_operations", ["user_id"], name: "index_curation_concerns_operations_on_user_id"
 
   create_table "presses", force: :cascade do |t|
     t.string   "name"
@@ -81,10 +77,9 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "role"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+    t.index ["user_id"], name: "index_roles_on_user_id"
   end
-
-  add_index "roles", ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  add_index "roles", ["user_id"], name: "index_roles_on_user_id"
 
   create_table "searches", force: :cascade do |t|
     t.text     "query_params"
@@ -92,9 +87,8 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "user_type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
-
-  add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "single_use_links", force: :cascade do |t|
     t.string   "downloadKey"
@@ -110,9 +104,8 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "proxy_for_type", null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.index ["proxy_for_id", "proxy_for_type"], name: "sipity_agents_proxy_for", unique: true
   end
-
-  add_index "sipity_agents", ["proxy_for_id", "proxy_for_type"], name: "sipity_agents_proxy_for", unique: true
 
   create_table "sipity_comments", force: :cascade do |t|
     t.integer  "entity_id",  null: false
@@ -120,11 +113,10 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["agent_id"], name: "index_sipity_comments_on_agent_id"
+    t.index ["created_at"], name: "index_sipity_comments_on_created_at"
+    t.index ["entity_id"], name: "index_sipity_comments_on_entity_id"
   end
-
-  add_index "sipity_comments", ["agent_id"], name: "index_sipity_comments_on_agent_id"
-  add_index "sipity_comments", ["created_at"], name: "index_sipity_comments_on_created_at"
-  add_index "sipity_comments", ["entity_id"], name: "index_sipity_comments_on_entity_id"
 
   create_table "sipity_entities", force: :cascade do |t|
     t.string   "proxy_for_global_id", null: false
@@ -132,11 +124,10 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.integer  "workflow_state_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.index ["proxy_for_global_id"], name: "sipity_entities_proxy_for_global_id", unique: true
+    t.index ["workflow_id"], name: "index_sipity_entities_on_workflow_id"
+    t.index ["workflow_state_id"], name: "index_sipity_entities_on_workflow_state_id"
   end
-
-  add_index "sipity_entities", ["proxy_for_global_id"], name: "sipity_entities_proxy_for_global_id", unique: true
-  add_index "sipity_entities", ["workflow_id"], name: "index_sipity_entities_on_workflow_id"
-  add_index "sipity_entities", ["workflow_state_id"], name: "index_sipity_entities_on_workflow_state_id"
 
   create_table "sipity_entity_specific_responsibilities", force: :cascade do |t|
     t.integer  "workflow_role_id", null: false
@@ -144,12 +135,11 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.integer  "agent_id",         null: false
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.index ["agent_id"], name: "sipity_entity_specific_responsibilities_agent"
+    t.index ["entity_id"], name: "sipity_entity_specific_responsibilities_entity"
+    t.index ["workflow_role_id", "entity_id", "agent_id"], name: "sipity_entity_specific_responsibilities_aggregate", unique: true
+    t.index ["workflow_role_id"], name: "sipity_entity_specific_responsibilities_role"
   end
-
-  add_index "sipity_entity_specific_responsibilities", ["agent_id"], name: "sipity_entity_specific_responsibilities_agent"
-  add_index "sipity_entity_specific_responsibilities", ["entity_id"], name: "sipity_entity_specific_responsibilities_entity"
-  add_index "sipity_entity_specific_responsibilities", ["workflow_role_id", "entity_id", "agent_id"], name: "sipity_entity_specific_responsibilities_aggregate", unique: true
-  add_index "sipity_entity_specific_responsibilities", ["workflow_role_id"], name: "sipity_entity_specific_responsibilities_role"
 
   create_table "sipity_notifiable_contexts", force: :cascade do |t|
     t.integer  "scope_for_notification_id",   null: false
@@ -158,12 +148,11 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.integer  "notification_id",             null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.index ["notification_id"], name: "sipity_notifiable_contexts_notification_id"
+    t.index ["scope_for_notification_id", "scope_for_notification_type", "reason_for_notification", "notification_id"], name: "sipity_notifiable_contexts_concern_surrogate", unique: true
+    t.index ["scope_for_notification_id", "scope_for_notification_type", "reason_for_notification"], name: "sipity_notifiable_contexts_concern_context"
+    t.index ["scope_for_notification_id", "scope_for_notification_type"], name: "sipity_notifiable_contexts_concern"
   end
-
-  add_index "sipity_notifiable_contexts", ["notification_id"], name: "sipity_notifiable_contexts_notification_id"
-  add_index "sipity_notifiable_contexts", ["scope_for_notification_id", "scope_for_notification_type", "reason_for_notification", "notification_id"], name: "sipity_notifiable_contexts_concern_surrogate", unique: true
-  add_index "sipity_notifiable_contexts", ["scope_for_notification_id", "scope_for_notification_type", "reason_for_notification"], name: "sipity_notifiable_contexts_concern_context"
-  add_index "sipity_notifiable_contexts", ["scope_for_notification_id", "scope_for_notification_type"], name: "sipity_notifiable_contexts_concern"
 
   create_table "sipity_notification_recipients", force: :cascade do |t|
     t.integer  "notification_id",    null: false
@@ -171,31 +160,28 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "recipient_strategy", null: false
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.index ["notification_id", "role_id", "recipient_strategy"], name: "sipity_notifications_recipients_surrogate"
+    t.index ["notification_id"], name: "sipity_notification_recipients_notification"
+    t.index ["recipient_strategy"], name: "sipity_notification_recipients_recipient_strategy"
+    t.index ["role_id"], name: "sipity_notification_recipients_role"
   end
-
-  add_index "sipity_notification_recipients", ["notification_id", "role_id", "recipient_strategy"], name: "sipity_notifications_recipients_surrogate"
-  add_index "sipity_notification_recipients", ["notification_id"], name: "sipity_notification_recipients_notification"
-  add_index "sipity_notification_recipients", ["recipient_strategy"], name: "sipity_notification_recipients_recipient_strategy"
-  add_index "sipity_notification_recipients", ["role_id"], name: "sipity_notification_recipients_role"
 
   create_table "sipity_notifications", force: :cascade do |t|
     t.string   "name",              null: false
     t.string   "notification_type", null: false
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.index ["name"], name: "index_sipity_notifications_on_name", unique: true
+    t.index ["notification_type"], name: "index_sipity_notifications_on_notification_type"
   end
-
-  add_index "sipity_notifications", ["name"], name: "index_sipity_notifications_on_name", unique: true
-  add_index "sipity_notifications", ["notification_type"], name: "index_sipity_notifications_on_notification_type"
 
   create_table "sipity_roles", force: :cascade do |t|
     t.string   "name",        null: false
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["name"], name: "index_sipity_roles_on_name", unique: true
   end
-
-  add_index "sipity_roles", ["name"], name: "index_sipity_roles_on_name", unique: true
 
   create_table "sipity_workflow_actions", force: :cascade do |t|
     t.integer  "workflow_id",                 null: false
@@ -203,11 +189,10 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.string   "name",                        null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.index ["resulting_workflow_state_id"], name: "sipity_workflow_actions_resulting_workflow_state"
+    t.index ["workflow_id", "name"], name: "sipity_workflow_actions_aggregate", unique: true
+    t.index ["workflow_id"], name: "sipity_workflow_actions_workflow"
   end
-
-  add_index "sipity_workflow_actions", ["resulting_workflow_state_id"], name: "sipity_workflow_actions_resulting_workflow_state"
-  add_index "sipity_workflow_actions", ["workflow_id", "name"], name: "sipity_workflow_actions_aggregate", unique: true
-  add_index "sipity_workflow_actions", ["workflow_id"], name: "sipity_workflow_actions_workflow"
 
   create_table "sipity_workflow_methods", force: :cascade do |t|
     t.string   "service_name",       null: false
@@ -215,55 +200,49 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.integer  "workflow_action_id", null: false
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.index ["workflow_action_id"], name: "index_sipity_workflow_methods_on_workflow_action_id"
   end
-
-  add_index "sipity_workflow_methods", ["workflow_action_id"], name: "index_sipity_workflow_methods_on_workflow_action_id"
 
   create_table "sipity_workflow_responsibilities", force: :cascade do |t|
     t.integer  "agent_id",         null: false
     t.integer  "workflow_role_id", null: false
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.index ["agent_id", "workflow_role_id"], name: "sipity_workflow_responsibilities_aggregate", unique: true
   end
-
-  add_index "sipity_workflow_responsibilities", ["agent_id", "workflow_role_id"], name: "sipity_workflow_responsibilities_aggregate", unique: true
 
   create_table "sipity_workflow_roles", force: :cascade do |t|
     t.integer  "workflow_id", null: false
     t.integer  "role_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["workflow_id", "role_id"], name: "sipity_workflow_roles_aggregate", unique: true
   end
-
-  add_index "sipity_workflow_roles", ["workflow_id", "role_id"], name: "sipity_workflow_roles_aggregate", unique: true
 
   create_table "sipity_workflow_state_action_permissions", force: :cascade do |t|
     t.integer  "workflow_role_id",         null: false
     t.integer  "workflow_state_action_id", null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.index ["workflow_role_id", "workflow_state_action_id"], name: "sipity_workflow_state_action_permissions_aggregate", unique: true
   end
-
-  add_index "sipity_workflow_state_action_permissions", ["workflow_role_id", "workflow_state_action_id"], name: "sipity_workflow_state_action_permissions_aggregate", unique: true
 
   create_table "sipity_workflow_state_actions", force: :cascade do |t|
     t.integer  "originating_workflow_state_id", null: false
     t.integer  "workflow_action_id",            null: false
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.index ["originating_workflow_state_id", "workflow_action_id"], name: "sipity_workflow_state_actions_aggregate", unique: true
   end
-
-  add_index "sipity_workflow_state_actions", ["originating_workflow_state_id", "workflow_action_id"], name: "sipity_workflow_state_actions_aggregate", unique: true
 
   create_table "sipity_workflow_states", force: :cascade do |t|
     t.integer  "workflow_id", null: false
     t.string   "name",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["name"], name: "index_sipity_workflow_states_on_name"
+    t.index ["workflow_id", "name"], name: "sipity_type_state_aggregate", unique: true
   end
-
-  add_index "sipity_workflow_states", ["name"], name: "index_sipity_workflow_states_on_name"
-  add_index "sipity_workflow_states", ["workflow_id", "name"], name: "sipity_type_state_aggregate", unique: true
 
   create_table "sipity_workflows", force: :cascade do |t|
     t.string   "name",                null: false
@@ -272,9 +251,8 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.boolean  "allows_access_grant"
+    t.index ["name"], name: "index_sipity_workflows_on_name", unique: true
   end
-
-  add_index "sipity_workflows", ["name"], name: "index_sipity_workflows_on_name", unique: true
 
   create_table "sub_brands", force: :cascade do |t|
     t.integer  "press_id",    null: false
@@ -283,10 +261,9 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["parent_id"], name: "index_sub_brands_on_parent_id"
+    t.index ["press_id"], name: "index_sub_brands_on_press_id"
   end
-
-  add_index "sub_brands", ["parent_id"], name: "index_sub_brands_on_parent_id"
-  add_index "sub_brands", ["press_id"], name: "index_sub_brands_on_press_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -302,10 +279,9 @@ ActiveRecord::Schema.define(version: 20170420185619) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "guest",                  default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "version_committers", force: :cascade do |t|
     t.string   "obj_id"
