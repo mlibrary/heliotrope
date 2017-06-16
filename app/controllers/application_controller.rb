2bacbc41edbf89a9f71a97addfed03d166cf6df9
Seth A. Johnson <sethajoh@umich.edu>
2017-06-16 16:45:53 -0400

    Resolves #894, Resolves #909, User login/logout routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
   # rescue_from ActiveFedora::ActiveFedoraError, with: :render_unauthorized
   rescue_from ActiveRecord::RecordNotFound, with: :render_unauthorized
 
+  # Remember where a user is to redirect to on login/logout
+  # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+  before_action :store_current_location, unless: :devise_controller?
+
   protected
 
     def render_unauthorized(_exception)
@@ -25,5 +29,25 @@ class ApplicationController < ActionController::Base
         format.html { render 'hyrax/base/unauthorized', status: :unauthorized }
         format.any { head :unauthorized, content_type: 'text/plain' }
       end
+    end
+
+  private
+
+    # override the devise helper to store the current location so we can
+    # redirect to it after loggin in or out. This override makes signing in
+    # and signing up work automatically.
+    def store_current_location
+      return unless request.get?
+      return if request.url =~ /image-service/
+      return if request.url =~ /downloads/
+      store_location_for(:user, request.url)
+    end
+
+    # override the devise method for where to go after signing out because theirs
+    # always goes to the root path. Because devise uses a session variable and
+    # the session is destroyed on log out, we need to use request.referrer
+    # root_path is there as a backup
+    def after_sign_out_path_for(_resource)
+      request.referer || root_path
     end
 end
