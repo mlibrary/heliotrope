a5bbb62006a827244a091611b9818bb89efe806c
gkostin1966 <gkostin@umich.edu>
2017-06-28 12:40:53 -0400

    importer

--- lib/import/monograph_builder.rb
+++ lib/import/monograph_builder.rb
@@ -2,22 +2,22 @@
 
 module Import
   class MonographBuilder
-    attr_reader :user, :attributes
-    delegate :curation_concern, to: :actor
+    attr_reader :user, :attributes, :curation_concern
 
     def initialize(user, attrs)
       @attributes = attrs
       @user = user
+      @curation_concern = nil
     end
 
     def run
-      actor.create(attributes)
+      @curation_concern = Monograph.new
+      actor.create(Hyrax::Actors::Environment.new(curation_concern, Ability.new(user), attributes))
     end
 
     private
 
       def actor
-        # @actor ||= Hyrax::CurationConcern.actor(Monograph.new, Ability.new(user))
         @actor ||= Hyrax::CurationConcern.actor
       end
   end
