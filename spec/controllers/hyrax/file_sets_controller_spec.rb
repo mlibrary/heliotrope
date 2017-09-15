cdf0f0db56d73a3084de73467c5fb9243193c515
Conor O'Malley <conorom@umich.edu>
2017-09-14 23:14:56 -0400

    simple redirection for FileSets

--- spec/controllers/hyrax/file_sets_controller_spec.rb
+++ spec/controllers/hyrax/file_sets_controller_spec.rb
@@ -3,19 +3,19 @@
 require 'rails_helper'
 
 describe Hyrax::FileSetsController do
+  let(:user) { create(:platform_admin) }
+  let(:press) { create(:press) }
+  let(:monograph) { create(:monograph, user: user, press: press.subdomain) }
+  let(:file_set) { create(:file_set) }
+
+  before do
+    monograph.ordered_members << file_set
+    monograph.save!
+    file_set.save!
+    sign_in user
+  end
+
   context 'tombstone' do
-    let(:user) { create(:platform_admin) }
-    let(:press) { create(:press) }
-    let(:monograph) { create(:monograph, user: user, press: press.subdomain) }
-    let(:file_set) { create(:file_set) }
-
-    before do
-      monograph.ordered_members << file_set
-      monograph.save!
-      file_set.save!
-      sign_in user
-    end
-
     context 'file set created' do
       before do
         get :show, params: { parent_id: monograph.id, id: file_set.id }
@@ -38,6 +38,14 @@ describe Hyrax::FileSetsController do
         # raise CanCan::AccessDenied currently redirects to root_url
         expect(response.header["Location"]).to match "http://test.host/"
       end
+    end
+  end
+
+  context 'orphan fileset with redirect_to' do
+    let(:orphan_file_set) { create(:file_set, redirect_to: file_set.id) }
+    it 'redirects to the approprite file_set in redirect_to' do
+      get :show, params: { id: orphan_file_set.id }
+      expect(response).to redirect_to Rails.application.routes.url_helpers.hyrax_file_set_path(file_set.id)
     end
   end
 end
