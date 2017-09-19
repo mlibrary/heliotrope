dba6f12c483211171dc50d2ddc1c6aa52f6eb48b
Greg Kostin <gkostin@umich.edu>
2017-09-19 15:50:09 -0400

    lib e_pub require dependency

--- lib/e_pub.rb
+++ lib/e_pub.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module EPub
-  extend ActiveSupport::Autoload
-
   #
   # Logger
   #
@@ -48,3 +46,18 @@ module EPub
     yield self
   end
 end
+
+#
+# Require Relative
+#
+require_relative './e_pub/cache'
+require_relative './e_pub/chapter'
+require_relative './e_pub/chapter_null_object'
+require_relative './e_pub/chapter_presenter'
+require_relative './e_pub/paragraph'
+require_relative './e_pub/paragraph_null_object'
+require_relative './e_pub/paragraph_presenter'
+require_relative './e_pub/presenter'
+require_relative './e_pub/publication'
+require_relative './e_pub/publication_null_object'
+require_relative './e_pub/publication_presenter'
