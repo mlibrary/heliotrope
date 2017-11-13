a18568a0036273211ff0f9113c20ab5ceab1f33a
Conor O'Malley <conorom@umich.edu>
2017-11-13 11:32:24 -0500

    tidy up media partials; PDF becomes default

--- spec/presenters/hyrax/file_set_presenter_spec.rb
+++ spec/presenters/hyrax/file_set_presenter_spec.rb
@@ -372,4 +372,56 @@ RSpec.describe Hyrax::FileSetPresenter do
       it { is_expected.to eq 'Download' }
     end
   end
+
+  describe '#heliotrope_media_partial' do
+    subject { presenter.heliotrope_media_partial }
+    let(:external_resource) { 'no' }
+    let(:fileset_doc) { SolrDocument.new(mime_type_ssi: mime_type, external_resource_ssim: external_resource) }
+    context "with an image" do
+      let(:mime_type) { 'image/tiff' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/leaflet_image' }
+    end
+    context "with a video" do
+      let(:mime_type) { 'video/webm' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/video' }
+    end
+    context "with an audio" do
+      let(:mime_type) { 'audio/wav' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/audio' }
+    end
+    context "with a pdf" do
+      let(:mime_type) { 'application/pdf' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/default' }
+    end
+    context "with a word document" do
+      let(:mime_type) { 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/default' }
+    end
+    context "with any other file" do
+      let(:mime_type) { 'application/binary' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/default' }
+    end
+    context "with an external resource" do
+      let(:mime_type) { nil }
+      let(:external_resource) { 'yes' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display/external_resource' }
+    end
+  end
+
+  describe '#heliotrope_media_partial for embedded assets' do
+    subject { presenter.heliotrope_media_partial('media_display_embedded') }
+    let(:fileset_doc) { SolrDocument.new(mime_type_ssi: mime_type, external_resource_ssim: 'no') }
+    context "with an image" do
+      let(:mime_type) { 'image/tiff' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display_embedded/leaflet_image' }
+    end
+    context "with a video" do
+      let(:mime_type) { 'video/webm' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display_embedded/video' }
+    end
+    context "with any other file" do
+      let(:mime_type) { 'application/binary' }
+      it { is_expected.to eq 'hyrax/file_sets/media_display_embedded/default' }
+    end
+  end
 end
