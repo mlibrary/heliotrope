9b63b3b14047f9c586559271cb7e5cc0ff05ef8c
moseshll <moseshll@umich.edu>
2017-11-27 18:33:51 -0500

    Resolves #1181 add metatags to EPUB reader.

--- app/controllers/e_pubs_controller.rb
+++ app/controllers/e_pubs_controller.rb
@@ -2,16 +2,20 @@
 
 class EPubsController < ApplicationController
   def show
-    presenter = Hyrax::FileSetPresenter.new(SolrDocument.new(FileSet.find(params[:id]).to_solr), current_ability, request)
-    if presenter.epub?
+    @presenter = Hyrax::FileSetPresenter.new(SolrDocument.new(FileSet.find(params[:id]).to_solr), current_ability, request)
+    if @presenter.epub?
       FactoryService.e_pub_publication(params[:id]) # cache epub
-      @title = presenter.parent.present? ? presenter.parent.title : presenter.title
-      @citable_link = presenter.citable_link
-      @creator_given_name = presenter.creator_given_name
-      @creator_family_name = presenter.creator_family_name
-      @back_link = params[:publisher].present? ? URI.join(main_app.root_url, params[:publisher]).to_s : main_app.monograph_catalog_url(presenter.monograph_id)
-      @subdomain = presenter.monograph.subdomain
+      @title = @presenter.parent.present? ? @presenter.parent.title : @presenter.title
+      @citable_link = @presenter.citable_link
+      @creator_given_name = @presenter.creator_given_name
+      @creator_family_name = @presenter.creator_family_name
+      @back_link = params[:publisher].present? ? URI.join(main_app.root_url, params[:publisher]).to_s : main_app.monograph_catalog_url(@presenter.monograph_id)
+      @subdomain = @presenter.monograph.subdomain
       @search_url = main_app.epub_search_url(params[:id], q: "").gsub!(/locale=en&/, '')
+      @monograph_presenter = nil
+      if @presenter.parent.present?
+        @monograph_presenter = Hyrax::PresenterFactory.build_presenters([@presenter.parent.id], Hyrax::MonographPresenter, current_ability).first
+      end
       render layout: false
     else
       Rails.logger.info("EPubsController.show(#{params[:id]}) is not an EPub.")
