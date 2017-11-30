23d38d02a437153ecd1e8e57e9b9e80f885c521b
Seth A. Johnson <sethajoh@umich.edu>
2017-11-30 15:15:02 -0500

    Resolves #1363, Full-text search improvements in the EBook Reader
    * Full sentences only in the context snippet from the search term.
    * Added a pretty simple attempt at "developing exact term matching"
    
    Also included (via an update Gemfile) are cozy-sun-bear changes that:
    
    * Add chapter title to search results
    * Display "No search results" message if there are no search results
    
    See https://github.com/mlibrary/cozy-sun-bear/pull/109

--- lib/e_pub/snippet.rb
+++ lib/e_pub/snippet.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require 'pragmatic_segmenter'
+
+# This class is supposed to show the search term in context, so surrounded
+# by sentences (not sentence fragments). It's unfortunatly a little involved...
+
 module EPub
   class Snippet
     private_class_method :new
     attr_accessor :node, :pos0, :pos1
 
-    SNIPPET_PAD = 50
+    SNIPPET_LENGTH = 50
 
     def self.from(node, pos0, pos1)
       if node.try(:text?) && pos0.integer? && pos1.integer?
@@ -20,59 +25,109 @@ module EPub
     end
 
     def snippet
-      "...#{before}#{@node.text[@pos0..@pos1]}#{after}..."
+      # We're only going to get at most 3 sentences for context with the middle
+      # sentence containing the search term. Some people write really long sentences
+      # though... not sure how great this will be...
+      sentences = parse_sentences(parse_fragments(parent_paragraph(@node)))
+      sentences.join(" ")
+    end
+
+    def parent_paragraph(current_node)
+      until current_node.name == "p" || current_node.name == "body"
+        current_node = current_node.parent
+      end
+      current_node
+    end
+
+    def parse_fragments(para, fragments = [])
+      para.children.each do |child|
+        if child.text?
+          fragment = OpenStruct.new
+          fragment.text = child.text
+          # This is really cheesy, adding the string "{{{HIT}}}"
+          # to track the fragment with the actual search string
+          # in it compared to the text of the rest of the paragraph.
+          # We need a way to know which sentence the search query
+          # is in without using markup/nokogiri.
+          fragment.text.insert(@pos0 - 1, "{{{HIT}}}") if child == @node
+          fragments << fragment
+        elsif child.name == "sup" && child.children[0].name == "a"
+          # Footnotes seem to really mess up sentence determinations, so skip them
+          # These might be marked up differently in different epubs so this is a WIP
+          # http://www.idpf.org/epub/30/spec/epub30-contentdocs.html#sec-contentdocs-vocab-association
+          next
+        else
+          parse_fragments(child, fragments)
+        end
+      end
+      fragments.map(&:text).join("")
+    end
+
+    def parse_sentences(text)
+      sentences = PragmaticSegmenter::Segmenter.new(text: text).segment
+      rvalue = []
+      sentences.each_index do |i|
+        if sentences[i].match?(/\{\{\{HIT\}\}\}/)
+          sentences[i].gsub!(/\{\{\{HIT\}\}\}/, '')
+          rvalue = determine_size(sentences, i)
+        end
+      end
+      rvalue
+    end
+
+    def determine_size(sentences, i)
+      # Trying to determine the right size snippet and which sentences
+      # out of a possible 3 to return. Fairly terrible.
+      first = sentences[i - 1]
+      hit   = sentences[i]
+      last  = sentences[i + 1]
+      if sentences.length == 1
+        [hit]
+      elsif hit.length > SNIPPET_LENGTH
+        [hit]
+      elsif sentences.length == 2
+        if last.nil?
+          [first, hit]
+        else
+          [hit, last]
+        end
+      else
+        [first, hit, last]
+      end
     end
 
     private
-
-      def after
-        text = @node.text[@pos1 + 1..@node.text.length] || ""
-        text += after_sibling_text
-        text[0..SNIPPET_PAD]
-      end
-
-      def after_sibling_text
-        if @node.next_sibling.present? && @node.next_sibling.text
-          @node.next_sibling.text
-        elsif @node.parent.next_sibling.present? && @node.parent.next_sibling.text
-          @node.parent.next_sibling
-        else
-          ""
-        end
-      end
-
-      def before
-        text = if @pos0.zero?
-                 ""
-               else
-                 @node.text[0..@pos0 - 1]
-               end
-
-        text = before_sibling_text + text
-
-        front = if text.length < SNIPPET_PAD
-                  text.length
-                else
-                  SNIPPET_PAD
-                end
-
-        text[text.length - front..text.length]
-      end
-
-      def before_sibling_text
-        if @node.previous_sibling.present? && @node.previous_sibling.text
-          @node.previous_sibling.text
-        elsif @node.parent.previous_sibling.present? && @node.parent.previous_sibling.text
-          @node.parent.previous_sibling.text
-        else
-          ""
-        end
-      end
 
       def initialize(node, pos0, pos1)
         @node = node
         @pos0 = pos0
         @pos1 = pos1
       end
+  end
+
+  class SnippetNullObject < Snippet
+    attr_reader :node, :pos0, :pos1
+
+    def initialize
+      @node = Nokogiri::XML(nil)
+      @pos0 = ""
+      @pos1 = ""
+    end
+
+    def snippet
+      ""
+    end
+
+    def parse_sentences
+      []
+    end
+
+    def parent_paragraph
+      Nokogiri::XML("<html><p></p></html>").xpath("//p")[0]
+    end
+
+    def parse_fragments
+      ""
+    end
   end
 end
