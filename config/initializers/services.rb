028e458131a29291b1551201bd111f892d6fcaae
Greg Kostin <gkostin@umich.edu>
2018-03-15 17:01:28 -0400

    Resolves #1625: Install new Authentication System (Keycard)

---
+++ config/initializers/services.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# PRESENTERS = {
+#     Listing => [ListingPresenter, ListingPolicy],
+#     User    => [UserPresenter, Vizier::ReadOnlyPolicy],
+#     'Listing::ActiveRecord_Relation' => [ListingsPresenter, ListingsPolicy],
+# }
+#
+# if Heliotrope.config.cache_presenters
+#   config_class = Vizier::CachingPresenterConfig
+# else
+#   config_class = Vizier::PresenterConfig
+# end
+#
+# if Heliotrope.config.checkpoint&.database
+#   Checkpoint::DB.config.opts = Heliotrope.config.checkpoint.database
+# end
+
+if Settings.keycard&.database
+  Keycard::DB.config.opts = Settings.keycard.database
+end
+
+# Services = Canister.new
+# Services.register(:presenters) {
+#   Vizier::PresenterFactory.new(PRESENTERS, config_type: config_class)
+# }
+#
+# Services.register(:checkpoint) { Checkpoint::Authority.new(agent_resolver: AgentResolver.new) }
