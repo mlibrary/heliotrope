f5cadbc4b81c57e58032b8ee5d06bd3f8d11d262
Seth A. Johnson <sethajoh@umich.edu>
2018-04-24 16:53:22 -0400

    Added webgl support for the unpack job
    See #1692

--- spec/jobs/unpack_job_spec.rb
+++ spec/jobs/unpack_job_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe UnpackJob, type: :job do
         expect(File.exist?(File.join(@root_path, epub.id + '.db'))).to be true
       end
     end
+
+    context "with a webgl" do
+      let(:webgl) { create(:file_set, content: File.open(File.join(fixture_path, 'fake-game.zip'))) }
+
+      it "unzips the webgl" do
+        described_class.perform_now(webgl.id, 'webgl')
+        @root_path = Hyrax::DerivativePath.new(webgl.id).derivative_path + 'webgl'
+        expect(File.exist?(File.join(@root_path, "Build", "UnityLoader.js"))).to be true
+      end
+    end
   end
 end
