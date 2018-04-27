9baf2a94a26a601f191922bd316623fc17e492c1
conorom <conorom@umich.edu>
2018-04-27 19:09:29 -0400

    creator_display is an optional field

--- spec/forms/hyrax/monograph_form_spec.rb
+++ spec/forms/hyrax/monograph_form_spec.rb
@@ -56,8 +56,7 @@ describe Hyrax::MonographForm do
 
   describe 'required_fields' do
     subject { described_class.required_fields }
-    it { is_expected.to eq %i[title press creator_display creator_family_name creator_given_name description
-                              publisher date_created] }
+    it { is_expected.to eq %i[title press creator_family_name creator_given_name description publisher date_created] }
   end
 
   describe 'select_press' do
