37b36b52845fa71212999b07d3b996159a313f76
Jonathan McGlone <jmcglone@umich.edu>
2018-05-01 13:22:19 -0400

    finalize JS; update asset page markup to work with accessibleTabs function

--- app/assets/javascripts/application/tabs.js
+++ app/assets/javascripts/application/tabs.js
@@ -1,16 +1,19 @@
 $(document).on('turbolinks:load', function() {
-  // Store the tab last used on the monograph catalog page and amend it to URL
-  // if it exists.
+  // Store the monograph ID and tab ID last used on the monograph catalog page
+  // Amend the tab ID to URL if it exists.
   storeTab();
+  // Improve accessibility and keyboard functionality of tabs
+  // For monograph and asset pages
   accessibleTabs();
-  keyboardTabs();
-  //Improve accessibility of tabs on monograph and asset pages
 
 function storeTab() {
   // If monograph page, store the last used tab
   if ($('#main.monograph').length > 0) {
     // https://stackoverflow.com/a/10524697
-    var monograph_id = '<%= @monograph_presenter.id %>';
+    var page_url = window.location.pathname.split( '/' );
+    // getting the monograph id from the URL path is not as reliable as using the
+    // monograph presenter
+    var monograph_id = page_url[3];
     var previous_monograph_id = localStorage.getItem('monograph_id');
     localStorage.setItem('monograph_id', monograph_id);
 
@@ -28,8 +31,6 @@ function storeTab() {
     // if not the first tab, unset aria semantics for first tab
     var lastTab = localStorage.getItem('lastTab');
     var firstTab = $('ul[role="tablist"]').children().first().children('a[role="tab"]').attr('href');
-    //.attr('href');
-    console.log(firstTab);
     if (lastTab) {
       $('a[href="' + lastTab + '"]').tab('show');
       $(lastTab).attr("aria-hidden", false);
@@ -43,95 +44,73 @@ function storeTab() {
 }
 
 function accessibleTabs() {
-  // a11y toggles for tab list items, fire on bootstrap tab events
-  // https://getbootstrap.com/docs/3.3/javascript/#tabs-events
-  // https://codepen.io/jwmcglone/pen/pVNLGB?editors=1010
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    var newTabList = $(e.target);
-    var oldTabList = $(e.relatedTarget);
-    var newTabPanel = $(e.target).attr('href');
-    var oldTabPanel = $(e.relatedTarget).attr('href');
-    // modify previously active tab list item and tab panel
-    // 1) change the old tabs values to aria-selected = false and aria-hidden = true
-    $(oldTabList).attr('aria-selected', false);
-    $(oldTabList).attr('tabindex', '-1');
-    $(oldTabPanel).attr('aria-hidden', true);
+  if ($('#tabs').length > 0) {
+    // Adapted from https://codepen.io/heydon/pen/veeaEa
+    var tabbed = document.querySelector('#tabs');
+    var tablist = tabbed.querySelector('ul');
+    var tabs = tablist.querySelectorAll('a');
+    var panels = tabbed.querySelectorAll('.tab-pane');
 
-    // modify newly active tab list item and tab panel
-    // 2) change the new tab values to aria-selected = true and aria-hidden = false
-    // 3) set the focus on the newly selected tab content .attr("tabindex", -1) and .focus();
-    $(newTabList).attr("aria-selected", true);
-    $(newTabList).attr('tabindex', '0');
-    $(newTabPanel).attr("aria-hidden", false);
-  });
-}
+    // The tab switching function
+    var switchTab = function switchTab(oldTab, newTab) {
+      $(newTab).tab('show');
+      newTab.focus();
+      // Make the active tab focusable by the user (Tab key)
+      newTab.removeAttribute('tabindex');
+      // Set the selected state
+      newTab.setAttribute('aria-selected', 'true');
+      newTab.parentNode.setAttribute('class', 'active');
+      oldTab.setAttribute('aria-selected', 'false');
+      oldTab.parentNode.removeAttribute('class', 'active');
+      oldTab.setAttribute('tabindex', '-1');
+      // Get the indices of the new and old tabs to find the correct
+      // tab panels to show and hide
+      var index = Array.prototype.indexOf.call(tabs, newTab);
+      var oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
+      panels[oldIndex].setAttribute('aria-hidden', 'true');
+      $(panels[index]).tab('show');
+      panels[index].setAttribute('aria-hidden', 'false');
+      //('class', 'active');
+    };
 
-function keyboardTabs() {
-  var tabbed = document.querySelector('#tabs');
-  var tablist = tabbed.querySelector('ul');
-  var tabs = tablist.querySelectorAll('a');
-  var panels = tabbed.querySelectorAll('.tab-pane');
+    // Add the tablist role to the first <ul> in the .tabbed container
+    tablist.setAttribute('role', 'tablist');
 
-  // The tab switching function
-  var switchTab = function switchTab(oldTab, newTab) {
-    $(newTab).tab('show');
-    newTab.focus();
-    // Make the active tab focusable by the user (Tab key)
-    newTab.removeAttribute('tabindex');
-    // Set the selected state
-    newTab.setAttribute('aria-selected', 'true');
-    newTab.parentNode.setAttribute('class', 'active');
-    oldTab.setAttribute('aria-selected', 'false');
-    oldTab.parentNode.removeAttribute('class', 'active');
-    oldTab.setAttribute('tabindex', '-1');
-    // Get the indices of the new and old tabs to find the correct
-    // tab panels to show and hide
-    var index = Array.prototype.indexOf.call(tabs, newTab);
-    var oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
-    //panels[oldIndex].setAttribute('class', 'active');
-    $(panels[index]).tab('show');
-    //('class', 'active');
-  };
+    // Add aria semantics - remove user focusability for each tab
+    Array.prototype.forEach.call(tabs, function (tab, i) {
+      tab.setAttribute('role', 'tab');
+      //tab.setAttribute('id', 'tab' + (i + 1));
+      tab.setAttribute('tabindex', '-1');
+      tab.parentNode.setAttribute('role', 'presentation');
 
-  // Add the tablist role to the first <ul> in the .tabbed container
-  tablist.setAttribute('role', 'tablist');
-
-  // Add semantics are remove user focusability for each tab
-  Array.prototype.forEach.call(tabs, function (tab, i) {
-    tab.setAttribute('role', 'tab');
-    //tab.setAttribute('id', 'tab' + (i + 1));
-    tab.setAttribute('tabindex', '-1');
-    tab.parentNode.setAttribute('role', 'presentation');
-
-    // Handle clicking of tabs for mouse users
-    tab.addEventListener('click', function (e) {
-      e.preventDefault();
-      var currentTab = tablist.querySelector('[aria-selected]');
-      if (e.currentTarget !== currentTab) {
-        switchTab(currentTab, e.currentTarget);
-      }
-    });
-
-    // Handle keydown events for keyboard users
-    tab.addEventListener('keydown', function (e) {
-      // Get the index of the current tab in the tabs node list
-      var index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-      // Work out which key the user is pressing and
-      // Calculate the new tab's index where appropriate
-      var dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-      if (dir !== null) {
+      // Handle clicking of tabs for mouse users
+      tab.addEventListener('click', function (e) {
         e.preventDefault();
-        // If the down key is pressed, move focus to the open panel,
-        // otherwise switch to the adjacent tab
-        dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-      }
-    });
-  });
+        var currentTab = tablist.querySelector('[aria-selected]');
+        if (e.currentTarget !== currentTab) {
+          switchTab(currentTab, e.currentTarget);
+        }
+      });
 
-  // Initially activate the first tab and reveal the first tab panel
-  tabs[0].removeAttribute('tabindex');
-  //tabs[0].setAttribute('aria-selected', 'true');
-  //panels[0].hidden = false;
+      // Handle keydown events for keyboard users
+      tab.addEventListener('keydown', function (e) {
+        // Get the index of the current tab in the tabs node list
+        var index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+        // Work out which key the user is pressing and
+        // Calculate the new tab's index where appropriate
+        var dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+        if (dir !== null) {
+          e.preventDefault();
+          // If the down key is pressed, move focus to the open panel,
+          // otherwise switch to the adjacent tab
+          dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
+        }
+      });
+    });
+
+    // Initially activate the first tab and reveal the first tab panel
+    tabs[0].removeAttribute('tabindex');
+  }
 }
 
 });
