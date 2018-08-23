b613a639a32366cbbaa77b82f4d8ab0be9f1af55
Conor O'Malley <conorom@umich.edu>
2018-08-23 12:22:32 -0400

    non-word hyphens: no errors or removal

--- spec/helpers/hyrax/citations_behaviors/title_behavior_spec.rb
+++ spec/helpers/hyrax/citations_behaviors/title_behavior_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe Hyrax::CitationsBehaviors::TitleBehavior do
   let(:title_behavior) { described_class.new }
 
+  # https://tools.lib.umich.edu/jira/browse/HELIO-1991
+
   it 'gets World War II right for Chicago' do
     expect(chicago_citation_title('World War II')).to eq 'World War II'
   end
@@ -27,5 +29,27 @@ RSpec.describe Hyrax::CitationsBehaviors::TitleBehavior do
 
   it 'gets quotes right for MLA' do
     expect(mla_citation_title('The Name &quot;Haydn&quot; In Quotes')).to eq 'The Name &quot;Haydn&quot; In Quotes'
+  end
+
+  # https://tools.lib.umich.edu/jira/browse/HELIO-2088
+
+  it "chicago_citation_title doesn't error 500 on single non-word hyphens, or remove them" do
+    expect(chicago_citation_title('Blah - The Meaning of same: dissections of Blah'))
+      .to eq 'Blah - The Meaning of Same: Dissections of Blah'
+  end
+
+  it "chicago_citation_title doesn't error 500 on double non-word hyphens, or remove them" do
+    expect(chicago_citation_title('Blah -- The Meaning of same: dissections of Blah'))
+      .to eq 'Blah -- The Meaning of Same: Dissections of Blah'
+  end
+
+  it "mla_citation_title doesn't error 500 on single non-word hyphens, or remove them" do
+    expect(mla_citation_title('Blah - The Meaning of same: dissections of Blah'))
+      .to eq 'Blah - The Meaning of Same: Dissections of Blah'
+  end
+
+  it "mla_citation_title doesn't error 500 on double non-word hyphens, or remove them" do
+    expect(mla_citation_title('Blah -- The Meaning of same: dissections of Blah'))
+      .to eq 'Blah -- The Meaning of Same: Dissections of Blah'
   end
 end
