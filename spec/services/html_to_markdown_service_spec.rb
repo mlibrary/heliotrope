d236ec69f4c902e351dba847b465b334317f4928
Conor O'Malley <conorom@umich.edu>
2018-12-19 18:54:28 -0500

    HELIO-2355: bypass unknown tags

--- spec/services/html_to_markdown_service_spec.rb
+++ spec/services/html_to_markdown_service_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe HtmlToMarkdownService do
-  describe '.markdown' do
+  describe '.convert' do
     let(:html) { "This is <em>italics</em> and this is <p>a paragraph</p><strong>bold</strong> a line<br>\nbreak and this is <del>strikethrough</del>" }
     let(:rvalue) { described_class.convert(html) }
 
@@ -14,6 +14,11 @@ RSpec.describe HtmlToMarkdownService do
     it 'converts links in the (inline) manner to which we have become accustomed' do
       expect(described_class.convert('Come read a book on <a href="http://www.fulcrum.org">www.fulcrum.org</a> right now!'))
         .to eq('Come read a book on [www.fulcrum.org](http://www.fulcrum.org) right now!')
+    end
+
+    it 'ignores unknown tags (like underline), but tries to convert their contents' do
+      expect(described_class.convert('Some stuff <u><crazytag>blah-de-blah-blah <i>something in italics</i></crazytag></u> waffle waffle!'))
+        .to eq('Some stuff blah-de-blah-blah _something in italics_ waffle waffle!')
     end
   end
 end
