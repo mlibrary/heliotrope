75295574f7d7519c5d7d780edc6daae3c73bc541
Seth A. Johnson <sethajoh@umich.edu>
2019-01-02 14:37:41 -0500

    HELIO-2384 Log view for share links

---
+++ app/controllers/share_link_logs_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ShareLinkLogsController < ApplicationController
+  def index
+    @share_link_logs = ShareLinkLog.filter(filtering_params(params)).order(created_at: :desc).page(params[:page])
+  end
+
+  private
+
+    def filtering_params(params)
+      params.slice(:ip_address_like, :institution_like, :press_like, :title_like, :noid_like, :token_like, :action_like, :created_like)
+    end
+end
--- app/models/share_link_log.rb
+++ app/models/share_link_log.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ShareLinkLog < ApplicationRecord
+  include Filterable
+
+  scope :ip_address_like, ->(like) { where("ip_address like ?", "%#{like}%") }
+  scope :institution_like, ->(like) { where("institution like ?", "%#{like}%") }
+  scope :press_like, ->(like) { where("subdomain like ?", "%#{like}%") }
+  scope :title_like, ->(like) { where("title like ?", "%#{like}%") }
+  scope :noid_like, ->(like) { where("noid like ?", "%#{like}%") }
+  scope :token_like, ->(like) { where("token like ?", "%#{like}%") }
+  scope :action_like, ->(like) { where("token like ?", "%#{like}%") }
+  scope :created_like, ->(like) { where("created_at like ?", "%#{like}") }
 end
