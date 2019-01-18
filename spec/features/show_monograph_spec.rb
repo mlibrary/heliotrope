96a481eb52cba1e9fb9db6ed04bbff7d90c9d333
moseshll <moseshll@umich.edu>
2019-01-18 15:38:14 -0500

    Addresses HELIO-2195 monograph show page redirection.

--- spec/features/show_monograph_spec.rb
+++ spec/features/show_monograph_spec.rb
@@ -5,10 +5,15 @@ require 'rails_helper'
 describe "Show a monograph" do
   let(:creator) { ['Shakespeare, William'] }
   let(:first_name) { 'William' }
+  let(:user) { create(:platform_admin) }
 
   let!(:monograph) { create(:public_monograph, creator: creator) }
   let!(:sipity_entity) do
     create(:sipity_entity, proxy_for_global_id: monograph.to_global_id.to_s)
+  end
+
+  before do
+    login_as user
   end
 
   it do
