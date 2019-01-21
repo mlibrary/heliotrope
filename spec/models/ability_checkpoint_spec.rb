c7b5cb831edc8cd80738d4a2c255c5708825dfd2
Seth A. Johnson <sethajoh@umich.edu>
2019-01-21 15:57:41 -0500

    HELIO-2442 Undefined method "platform_admin?" for Ability Checkpoint

--- spec/models/ability_checkpoint_spec.rb
+++ spec/models/ability_checkpoint_spec.rb
@@ -22,4 +22,31 @@ describe AbilityCheckpoint do
     it { expect(subject.current_user).to be current_user }
     it { is_expected.to be_able_to(:manage, :all) }
   end
+
+  context 'platform_admin' do
+    let(:current_user) { create(:platform_admin) }
+
+    describe "#platform_admin?" do
+      subject { described_class.new(current_user).platform_admin? }
+
+      it { is_expected.to be true }
+    end
+
+    describe "#admin?" do
+      subject { described_class.new(current_user).admin? }
+
+      it { is_expected.to be true }
+    end
+  end
+
+  context 'press_admin' do
+    let(:press) { create(:press) }
+    let(:current_user) { create(:press_admin, press: press) }
+
+    describe '#admin_for?' do
+      subject { described_class.new(current_user).admin_for?(press) }
+
+      it { is_expected.to be true }
+    end
+  end
 end
