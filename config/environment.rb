0d44371dcf93e8fe71ea54638ec57891614d0c7d
Seth A. Johnson <sethajoh@umich.edu>
2019-03-28 14:56:19 -0400

    HELIO-2068 Set production log levels

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Contrary to Rails documentation, this is the only way I could set the log levels
+# See HELIO-2068
+Rails.logger.level = Settings.log_level || 0
+Rails.logger.warn("LOG LEVEL IS SET IN environment.rb: #{Rails.logger.level}")
