f5828d749eaa6cb47135fd978f832e565aae01e1
Esty Thomas <esty@umich.edu>
2019-04-17 11:59:36 -0400

    MonographPresenter.monograph_thumbnail generates entire img tag
    The thumbnails for real covers are resized by the image service but the
    generic work icon for monographs without covers was turning up at very
    large sizes sometimes. By moving the img tag inside the
    monograph_thumbnail method (which was generating the img src) we are
    able to add a max-width to the generic icon of whatever width we'd
    request for the real cover.

--- app/presenters/hyrax/monograph_presenter.rb
+++ app/presenters/hyrax/monograph_presenter.rb
@@ -250,11 +250,14 @@ module Hyrax
     end
 
     def monograph_thumbnail(width = 225)
-      if representative_id.present?
-        "/image-service/#{representative_id}/full/#{width},/0/default.jpg#{representative_presenter&.image_cache_breaker}"
-      else
-        thumbnail_path
-      end
+      img_tag = "<img class=\"img-responsive\" src="
+      img_tag += if representative_id.present?
+                   "\"/image-service/#{representative_id}/full/#{width},/0/default.jpg#{representative_presenter&.image_cache_breaker}\""
+                 else
+                   "\"#{thumbnail_path}\" style=\"max-width:#{width}px\""
+                 end
+      img_tag += " alt=\"Cover image for #{representative_alt_text}\">"
+      img_tag
     end
 
     # This overrides CC 1.6.2's work_show_presenter.rb which is recursive.
