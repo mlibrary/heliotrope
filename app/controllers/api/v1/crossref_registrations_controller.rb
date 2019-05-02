16d2f45b0f1aee1e22b53eba8d8f7d7ca2088990
Seth A. Johnson <sethajoh@umich.edu>
2019-05-02 14:48:36 -0400

    HELIO-2655 Add crossref submission api endpoint

---
+++ app/controllers/api/v1/crossref_registrations_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    class CrossrefRegistrationsController < API::ApplicationController
+      def create
+        file = params[:fname].read
+        resp = Crossref::Register.new(file).post
+
+        if resp.code == 200 && resp.body.match(/SUCCESS/)
+          render json: { body: resp.body }, status: :ok
+        else
+          # Crossref will often send back 200 :ok status codes
+          # even if there are problems. It seems like the only reliable
+          # indication there's an error is in the html body. Which is
+          # not great.
+          render json: { body: resp.body }, status: :bad_request
+        end
+      end
+
+      private
+
+        # Only log exceptions, we do crossref specific logging
+        # in the CrossrefSubmissionLog from Crossref::Register
+        def log_request_response(exception = nil)
+          super(exception) if exception.present?
+        end
+
+        def crossref_registrations_params
+          params.require(:crossref_registrations).permit(:fname)
+        end
+    end
+  end
+end
