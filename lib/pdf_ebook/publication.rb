46d610bb7683bcd59c7e0732e0db3573f4d03fd5
Conor O'Malley <conorom@umich.edu>
2019-08-07 16:42:15 -0400

    Origami::Reference issue

--- lib/pdf_ebook/publication.rb
+++ lib/pdf_ebook/publication.rb
@@ -46,11 +46,21 @@ module PDFEbook
       end
 
       # Takes Origami::OutlineItem and 1-based depth
-      def iterate_outlines(outline, depth)
+      def iterate_outlines(outline, depth) # rubocop:disable Metrics/CyclomaticComplexity
         intervals = []
         until outline.nil?
           page = nil
-          page = outline&.[](:A)&.solve&.[](:D)&.[](0)&.solve # Origami::Page
+          page = outline&.[](:A)&.solve&.[](:D)
+          if page.is_a?(Origami::Reference) # skips external links
+            begin
+              target = page.solve
+            rescue Origami::InvalidReferenceError
+              outline = outline[:Next]&.solve
+              next
+            end
+            page = target
+          end
+          page = page&.[](0)&.solve # gets to Origami::Page
           page ||= outline[:Dest]&.solve&.[](0)&.solve
           unless page.nil?
             page_number = @obj_to_page[page.no] || 0
