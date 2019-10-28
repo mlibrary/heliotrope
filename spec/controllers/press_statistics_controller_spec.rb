0531157de68173bc417f0a87989b00f08a5a3b76
moseshll <moseshll@umich.edu>
2019-10-28 16:30:24 -0400

    Addresses HELIO-2983 make analytics world-visible.

---
+++ spec/controllers/press_statistics_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PressStatisticsController, type: :controller do
+  describe '#index' do
+    context 'a press' do
+      let(:press) { create :press }
+
+      before do
+        get :index, params: { press: press.subdomain }
+      end
+
+      it 'is successful' do
+        expect(response).to be_success
+        expect(response).to render_template('press_statistics/index')
+      end
+    end
+  end
+end
