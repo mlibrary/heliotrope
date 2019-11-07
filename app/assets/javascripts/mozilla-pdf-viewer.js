71007d4202bd9d49fce20f35bc34a7f02327bc8c
conorom <conorom@umich.edu>
2019-11-07 17:28:12 -0500

    need js.cookie for GDPR checks

--- app/assets/javascripts/mozilla-pdf-viewer.js
+++ app/assets/javascripts/mozilla-pdf-viewer.js
@@ -1,6 +1,7 @@
 // A place to put mozilla-pdf-viwer-specific scripts
 //
 //= require jquery
+//= require js.cookie
 // needed by social media share links widget (data-toggle="dropdown")
 //= require bootstrap/dropdown
 //= require cozy-sun-bear-main
