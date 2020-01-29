a27cc00aa2dda0fd18f1cf643ded58c6793e508b
Conor O'Malley <conorom@umich.edu>
2020-01-29 13:34:11 -0500

    more focused facet-panel-toggle event

--- app/assets/javascripts/application/role_button_click_on_enter.js
+++ app/assets/javascripts/application/role_button_click_on_enter.js
@@ -1,5 +1,6 @@
+// Allow enter key to open the facet panels (a11y). See also https://tools.lib.umich.edu/jira/browse/CSB-248
 $(document).on('turbolinks:load', function() {
-  $(document).on('keyup', function (event) {
+  $('.panel-heading.facet-field-heading.collapse-toggle').on('keyup', function (event) {
     if ((event.keyCode === 13) && ($(document.activeElement).attr('role') === 'button')) {
       event.preventDefault();
       document.activeElement.click();
