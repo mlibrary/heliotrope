bfdafa1d0b90200b8730874c4649c8bc05ce3142
Greg Kostin <gkostin@umich.edu>
2020-03-03 11:04:02 -0500

    Resolves HELIO-3171 default sort on catalog page.

--- app/models/press_search_builder.rb
+++ app/models/press_search_builder.rb
@@ -9,4 +9,13 @@ class PressSearchBuilder < ::SearchBuilder
     all_presses = children.push(blacklight_params['press']).uniq
     solr_parameters[:fq] << "{!terms f=press_sim}#{all_presses.map(&:downcase).join(',')}"
   end
+
+  def default_sort_field
+    case blacklight_params['press']
+    when /^barpublishing$/i
+      blacklight_config.sort_fields['year desc'] # Sort by Publication Date (Newest First)
+    else
+      blacklight_config.default_sort_field
+    end
+  end
 end
--- spec/models/search_builder_spec.rb
+++ spec/models/search_builder_spec.rb
@@ -37,4 +37,35 @@ describe SearchBuilder do
       expect(subject['facet.field']).to include 'subject_sim'
     end
   end
+
+  context 'sort field' do
+    let(:default_sort_field) { double('default_sort_field') }
+
+    before { allow(config).to receive(:default_sort_field).and_return(default_sort_field) }
+
+    describe '#default_sort_field' do
+      subject { search_builder.default_sort_field }
+
+      it { is_expected.to be default_sort_field }
+    end
+
+    describe "#sort" do
+      subject { search_builder.sort }
+
+      let(:field) { double('field') }
+
+      before { allow(default_sort_field).to receive(:sort).and_return(field) }
+
+      it { is_expected.to be field }
+
+      context 'param sort' do
+        let(:blacklight_params) { { sort: param_field } }
+        let(:param_field) { double('param_field') }
+
+        before { allow(search_builder).to receive(:blacklight_params).and_return(blacklight_params) }
+
+        it { is_expected.to be param_field }
+      end
+    end
+  end
 end
