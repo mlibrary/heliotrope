75b33e9a8d9fe77ea2b725d302343b2ba1d981e5
Conor O'Malley <conorom@umich.edu>
2021-07-20 00:07:00 -0400

    add examples for multiple filenames and basenames

--- spec/services/embed_code_service_spec.rb
+++ spec/services/embed_code_service_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe EmbedCodeService do
     let(:video_embed_url) { Rails.application.routes.url_helpers.embed_url(hdl: HandleNet.path(video.id)) }
     let(:interactive_map_embed_url) { Rails.application.routes.url_helpers.embed_url(hdl: HandleNet.path(interactive_map.id)) }
 
-
     let(:cover_embed_attributes) { "div[@data-href=\"#{cover_embed_url}\"][@data-title=\"#{cover.title.first}\"][@data-resource-type=\"image\"]" }
     let(:epub_embed_attributes) { "[@data-href=\"#{epub_embed_url}\"][@data-title=\"#{epub.title.first}\"][@data-resource-type=\"resource\"]" }
     let(:image_embed_attributes) { "[@data-href=\"#{image_embed_url}\"][@data-title=\"#{image.title.first}\"][@data-resource-type=\"image\"]" }
@@ -104,6 +103,91 @@ RSpec.describe EmbedCodeService do
         expect(doc.search(interactive_map_embed_attributes).size).to eq(1)
         # check parent `p.image` tags are changed to <div> tags
         expect(doc.search("div[@class='image']").count).to eq(4)
+      end
+
+      context "Monograph has more than one filename matching the EPUB file references" do
+        let(:image_same_filename) { create(:file_set, label: 'image.jpg') }
+        let(:audio_same_filename) { create(:file_set, label: 'audio.mp3') }
+        let(:video_same_filename) { create(:file_set, label: 'video.mp4') }
+        let(:interactive_map_same_filename) { create(:file_set, label: 'interactive_map.zip', resource_type: ['interactive map']) }
+
+        before do
+          ordered_members = [cover, epub, image, image_same_filename, audio, audio_same_filename, video,
+                             video_same_filename, interactive_map, interactive_map_same_filename]
+          monograph.ordered_members = ordered_members
+          monograph.save!
+          ordered_members.each { |item| item.save! }
+          UnpackJob.perform_now(epub.id, 'epub')
+        end
+
+        it "Does not insert embed codes for files referenced in the EPUB using data attributes" do
+          expect(File.exist?(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_data_attributes.xhtml'))).to be true
+          doc = Nokogiri::XML(File.read(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_data_attributes.xhtml')))
+          expect(doc.search(image_embed_attributes)).to be_empty
+          expect(doc.search(audio_embed_attributes)).to be_empty
+          expect(doc.search(video_embed_attributes)).to be_empty
+          expect(doc.search(interactive_map_embed_attributes)).to be_empty
+        end
+
+        it "Does not insert embed codes for files referenced in the EPUB using img src basename matching" do
+          expect(File.exist?(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_img_src_basenames.xhtml'))).to be true
+          doc = Nokogiri::XML(File.read(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_img_src_basenames.xhtml')))
+          expect(doc.search(image_embed_attributes)).to be_empty
+          expect(doc.search(audio_embed_attributes)).to be_empty
+          expect(doc.search(video_embed_attributes)).to be_empty
+          expect(doc.search(interactive_map_embed_attributes)).to be_empty
+          # check parent `p.image` tags are *not* changed to <div> tags
+          expect(doc.search("p[@class='image']").count).to eq(4)
+        end
+      end
+
+      context "Monograph has more than one file basename matching the EPUB file references" do
+        let(:image_same_basename) { create(:file_set, label: 'image.png') }
+        let(:audio_same_basename) { create(:file_set, label: 'audio.ogg') }
+        let(:video_same_basename) { create(:file_set, label: 'video.webm') }
+        let(:interactive_map_same_basename) { create(:file_set, label: 'interactive_map.zipx', resource_type: ['interactive map']) }
+
+        # mime_type is indexed by jobs that are not run here, these lines give presenters that delegate to Solr docs...
+        # which can correctly respond to `HeliotropeMimeTypes` methods. Several presenter methods require these also.
+        let(:image_same_basename_presenter) { Hyrax::FileSetPresenter.new(SolrDocument.new(image_same_basename.to_solr.merge(mime_type_ssi: 'image/png')), nil) }
+        let(:audio_same_basename_presenter) { Hyrax::FileSetPresenter.new(SolrDocument.new(audio_same_basename.to_solr.merge(mime_type_ssi: 'audio/ogg')), nil) }
+        let(:video_same_basename_presenter) { Hyrax::FileSetPresenter.new(SolrDocument.new(video_same_basename.to_solr.merge(mime_type_ssi: 'video/webm')), nil) }
+        # mime_type not needed here, the switch is on resource_type metadata value
+        let(:interactive_map_same_basename_presenter) { Hyrax::FileSetPresenter.new(SolrDocument.new(interactive_map_same_basename.to_solr), nil) }
+
+        before do
+          allow(Hyrax::PresenterFactory).to receive(:build_for).with(ids: [image.id], presenter_class: Hyrax::FileSetPresenter, presenter_args: nil).and_return([image_presenter])
+          allow(Hyrax::PresenterFactory).to receive(:build_for).with(ids: [audio.id], presenter_class: Hyrax::FileSetPresenter, presenter_args: nil).and_return([audio_presenter])
+          allow(Hyrax::PresenterFactory).to receive(:build_for).with(ids: [video.id], presenter_class: Hyrax::FileSetPresenter, presenter_args: nil).and_return([video_presenter])
+          allow(Hyrax::PresenterFactory).to receive(:build_for).with(ids: [interactive_map.id], presenter_class: Hyrax::FileSetPresenter, presenter_args: nil).and_return([interactive_map_presenter])
+
+          ordered_members = [cover, epub, image, image_same_basename, audio, audio_same_basename, video,
+                             video_same_basename, interactive_map, interactive_map_same_basename]
+          monograph.ordered_members = ordered_members
+          monograph.save!
+          ordered_members.each { |item| item.save! }
+          UnpackJob.perform_now(epub.id, 'epub')
+        end
+
+        it "Inserts embed codes for files referenced in the EPUB using data attributes" do
+          expect(File.exist?(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_data_attributes.xhtml'))).to be true
+          doc = Nokogiri::XML(File.read(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_data_attributes.xhtml')))
+          expect(doc.search(image_embed_attributes).size).to eq(1)
+          expect(doc.search(audio_embed_attributes).size).to eq(2) # one is a no-local-image example
+          expect(doc.search(video_embed_attributes).size).to eq(1)
+          expect(doc.search(interactive_map_embed_attributes).size).to eq(1)
+        end
+
+        it "Does not insert embed codes for files referenced in the EPUB using img src basename matching" do
+          expect(File.exist?(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_img_src_basenames.xhtml'))).to be true
+          doc = Nokogiri::XML(File.read(File.join(root_path, 'EPUB', 'xhtml', 'embeds_using_img_src_basenames.xhtml')))
+          expect(doc.search(image_embed_attributes)).to be_empty
+          expect(doc.search(audio_embed_attributes)).to be_empty
+          expect(doc.search(video_embed_attributes)).to be_empty
+          expect(doc.search(interactive_map_embed_attributes)).to be_empty
+          # check parent `p.image` tags are *not* changed to <div> tags
+          expect(doc.search("p[@class='image']").count).to eq(4)
+        end
       end
 
       context "EPUB embeds do not use correct casing on filenames or file basenames and/or have hyphens" do
