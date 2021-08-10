0b18d779ff24a031ec6d29cc9f2f3ebcdaa00984
Seth A. Johnson <sethajoh@umich.edu>
2021-08-10 14:47:46 -0400

    HELIO-3984 Refactor policies to compare institution affiliation with license affiliation

--- app/policies/ebook_operation.rb
+++ app/policies/ebook_operation.rb
@@ -20,8 +20,31 @@ class EbookOperation < ApplicationPolicy
     end
 
     def licensed_for?(entitlement)
-      authority
-        .licenses_for(actor, ebook)
-        .any? { |license| license.allows?(entitlement) }
+      if Incognito.developer?(actor) # TODO: remove
+        licenses = authority.licenses_for(actor, ebook)
+
+        return true if licenses
+                        .where(licensee_type: "Greensub::Individual")
+                        .any? { |license| license.allows?(entitlement) }
+
+        return true if licenses
+                        .where(licensee_type: "Greensub::Institution")
+                        .any? { |license| license.allows?(entitlement) } && affiliation_match?(licenses)
+        false
+      else
+        authority
+          .licenses_for(actor, ebook)
+          .to_a
+          .any? { |license| license.allows?(entitlement) }
+      end
     end
+
+    private
+
+      def affiliation_match?(licenses)
+        licenses.where(licensee_type: "Greensub::Institution").each do |license|
+          return true if (license.licensee.institution_affiliations.map(&:affiliation) & license.license_affiliations.map(&:affiliation)).present?
+        end
+        false
+      end
 end
