8a1d5fc97cf91686c76f92badf5a5420944ada0b
conorom <conorom@umich.edu>
2022-05-19 14:33:36 -0400

    realign CharacterizeJob with Hyrax

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -1,57 +1,36 @@
 # frozen_string_literal: true
 
-class CharacterizeJob < ApplicationJob
+##
+# a +ActiveJob+ job to process file characterization.
+#
+# the characterization process is handled by a service object, which is
+# configurable via {CharacterizeJob.characterization_service}.
+#
+# @example setting a custom characterization service
+#   class MyCharacterizer
+#     def run(file, path)
+#       # do custom characterization
+#     end
+#   end
+#
+#   # in a Rails initializer
+#   CharacterizeJob.characterization_service = MyCharacterizer.new
+# end
+class CharacterizeJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
 
+  class_attribute :characterization_service
+  self.characterization_service = Hydra::Works::CharacterizationService
+
+  # Characterizes the file at 'filepath' if available, otherwise, pulls a copy from the repository
+  # and runs characterization on that file.
   # @param [FileSet] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
-  def perform(file_set, file_id, filepath = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    filename = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath)
-    raise LoadError, "#{file_set.class.characterization_proxy} was not found" unless file_set.characterization_proxy?
-
-    # Override CC/Hyrax, see https://github.com/samvera/hyrax/issues/1152
-    # store this so we can tell if the original_file is changing
-    previous_checksum = file_set.original_file.original_checksum.first
-
-    # On file updates characterization adds new height/width/size/checksum values to an unorderable ActiveTriples::Relation on original_file
-    # We need accurate height/width for riiif and leaflet, and want as clean a "recharacterization" as possible
-    file_set.original_file.height = []
-    file_set.original_file.width  = []
-    file_set.original_file.original_checksum = []
-    file_set.original_file.file_size = []
-    file_set.original_file.format_label = []
-    # if the current FileSet title is the same as the label, it's a file name (as opposed to user-entered)...
-    # and we'll set it to the new file's name
-    reset_title = file_set.title.first == file_set.label
-
-    # Also in case of a file update, clear out the riiif cached base image if one exists
-    cached_file = Rails.root.join('tmp', 'network_files', Digest::MD5.hexdigest(file_set.original_file.uri.to_s))
-    File.delete(cached_file) if File.exist?(cached_file)
-
-    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filename)
-
-    Rails.logger.debug { "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})" }
-    file_set.characterization_proxy.save!
-
-    # Override CC/Hyrax, see https://github.com/samvera/hyrax/issues/1152
-    # Probably not ideal to make edits to the FileSet itself here, rather than just the characterization_proxy...
-    # (a.k.a. original_file for us). But these things make sense to do, and the job is asynchronous.
-    if file_set.original_checksum.first != previous_checksum
-      # actual file has changed, change the mod timestamp on the FileSet object (for APTrust bagging etc)
-      file_set.date_modified = Hyrax::TimeService.time_in_utc
-    end
-
-    # set title to label if that's how it was before this characterization
-    # for the encoding issue see https://tools.lib.umich.edu/jira/browse/HELIO-4226 (and Hyrax #5670 and #5671)
-    file_set.title = [file_set.characterization_proxy.original_name.force_encoding("UTF-8")] if reset_title
-
-    file_set.label = file_set.characterization_proxy.original_name
-    # save will do this now that we're updating the FileSet
-    # file_set.update_index
-    file_set.save!
-
-    file_set.parent&.in_collections&.each(&:update_index)
+  def perform(file_set, file_id, filepath = nil)
+    raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
+    filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
+    characterize(file_set, file_id, filepath)
 
     # Heliotrope addition: allow "reversioned" FileSets to be Unpacked if needed
     kind = FeaturedRepresentative.where(file_set_id: file_set.id)&.first&.kind
@@ -62,6 +41,73 @@ class CharacterizeJob < ApplicationJob
       UnpackJob.perform_later(file_set.id, 'interactive_map')
     end
 
-    CreateDerivativesJob.perform_later(file_set, file_id, filename)
+    CreateDerivativesJob.perform_later(file_set, file_id, filepath)
   end
+
+  private
+
+    def characterize(file_set, _file_id, filepath) # rubocop:disable Metrics/AbcSize
+      # store this so we can tell if the original_file is actually changing
+      previous_checksum = file_set.characterization_proxy.original_checksum.first
+
+      clear_metadata(file_set)
+
+      # If the current FileSet title is the same as the label, it must be a filename as opposed to a user-entered...
+      # value. So later we'll ensure it's set to the new file's filename.
+      reset_title = file_set.title.first == file_set.label
+
+      # Heliotrope addition over Hyrax 3 code to clear out the riiif cached base image if one exists
+      cached_file = Rails.root.join('tmp', 'network_files', Digest::MD5.hexdigest(file_set.original_file.uri.to_s))
+      File.delete(cached_file) if File.exist?(cached_file)
+
+      characterization_service.run(file_set.characterization_proxy, filepath)
+      Rails.logger.debug { "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})" }
+      file_set.characterization_proxy.alpha_channels = channels(filepath) if file_set.image? && Hyrax.config.iiif_image_server?
+      file_set.characterization_proxy.save!
+
+      # Ensure that if the actual file content has changed, the mod timestamp on the FileSet object changes.
+      # Otherwise this does not happen when rolling back to a previous version. Perhaps this should be set as part of...
+      # `FileActor.revert_to` (or its replacement Transaction?!), where the FileSet is saved. Not sure if the...
+      # before/after checksum is readily available there though. I like this checksum verification because it allows...
+      # all changes to the current FileSet version to be detected, which in our case triggers re-creation of a...
+      # "cold storage" archive of the parent Work. It's worth noting that adding a *new* version always touches this...
+      # mod time. This is done in the versioning code.
+      file_set.date_modified = Hyrax::TimeService.time_in_utc if file_set.characterization_proxy.original_checksum.first != previous_checksum
+
+      # set title to label if that's how it was before this characterization
+      # for the encoding issue see https://tools.lib.umich.edu/jira/browse/HELIO-4226 (and Hyrax #5670 and #5671)
+      file_set.title = [file_set.characterization_proxy.original_name.force_encoding("UTF-8")] if reset_title
+
+      # always set the label to the original_name
+      file_set.label = file_set.characterization_proxy.original_name
+
+      file_set.save!
+    end
+
+    def clear_metadata(file_set)
+      # The characterization of additional file versions adds new height/width/size/checksum values to un-orderable...
+      # `ActiveTriples::Relation` fields on `original_file`. Values from those are then randomly pulled into Solr...
+      # fields which may have scalar or vector cardinality. So for height/width you get two scalar values pulled from...
+      # "randomized parallel arrays". Upshot is to reset all of these before (re)characterization to stop the mayhem.
+      file_set.characterization_proxy.height = []
+      file_set.characterization_proxy.width  = []
+      file_set.characterization_proxy.original_checksum = []
+      file_set.characterization_proxy.file_size = []
+      file_set.characterization_proxy.format_label = []
+    end
+
+    def channels(filepath)
+      ch = MiniMagick::Tool::Identify.new do |cmd|
+        cmd.format '%[channels]'
+        cmd << filepath
+      end
+      [ch]
+    end
+
+    ##
+    # @api public
+    # @return [#run]
+    def characterization_service
+      self.class.characterization_service
+    end
 end
--- spec/jobs/characterize_job_spec.rb
+++ spec/jobs/characterize_job_spec.rb
@@ -3,22 +3,21 @@
 require 'rails_helper'
 require 'fakefs/spec_helpers'
 
-# Since we lifted this out of CC 1.6.2, we'll need to run the tests too
+# This is stolen from Hyrax so we'll periodically keep the spec updated with the Hyrax one, with our own additional...
+# specs following, as commented below
 
-describe CharacterizeJob do
-  # let(:file_set) { FileSet.new(id: file_set_id, title: ['previous_file.jpg'], label: 'previous_file.jpg', date_modified: 'old_mod_date', resource_type: [resource_type]) }
-  let(:resource_type) { 'resource_type' }
+describe CharacterizeJob, :clean_repo do
   let(:file_set_id) { 'abc12345' }
-  let(:label) { 'previous_file.jpg' }
+  let(:filename)    { Rails.root.join('tmp', 'uploads', 'ab', 'c1', '23', '45', 'abc12345', 'picture.png').to_s }
+  let(:label) { 'picture.png' }
   let(:title) { ['My User-Entered Title'] }
   let(:file_set) do
-    FileSet.new(id: file_set_id, title: title, label: label, date_modified: 'old_mod_date', resource_type: [resource_type]).tap do |fs|
+    FileSet.new(id: file_set_id, title: title, label: label, date_modified: 'old_mod_date').tap do |fs|
       allow(fs).to receive(:original_file).and_return(file)
       allow(fs).to receive(:update_index)
     end
   end
-  let(:file_path)   { Rails.root + 'tmp' + 'uploads' + 'ab' + 'c1' + '23' + '45' + 'abc12345' + 'picture.png' }
-  let(:filename)    { file_path.to_s }
+  # let(:io)          { JobIoWrapper.new(file_set_id: file_set.id, user: create(:user), path: filename) }
   let(:file) do
     Hydra::PCDM::File.new.tap do |f|
       f.content = 'foo'
@@ -27,14 +26,25 @@ describe CharacterizeJob do
       f.width = '222'
       f.file_size = '123456'
       f.format_label = ["Portable Network Graphics"]
-      f.original_checksum = ['asdf123456']
+      f.original_checksum = ['checksum123']
       f.save!
     end
   end
 
   before do
     allow(FileSet).to receive(:find).with(file_set_id).and_return(file_set)
-    allow(file_set).to receive(:original_file).and_return(file)
+    allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
+    allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+  end
+
+  context 'with valid filepath param' do
+    let(:filename) { File.join(fixture_path, 'world.png') }
+
+    it 'skips Hyrax::WorkingDirectory' do
+      expect(Hyrax::WorkingDirectory).not_to receive(:find_or_retrieve)
+      expect(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
+      described_class.perform_now(file_set, file.id, filename)
+    end
   end
 
   context 'when the characterization proxy content is present' do
@@ -49,129 +59,8 @@ describe CharacterizeJob do
 
   context 'when the characterization proxy content is absent' do
     before { allow(file_set).to receive(:characterization_proxy?).and_return(false) }
-
     it 'raises an error' do
-      expect { described_class.perform_now(file_set, file.id) }.to raise_error(LoadError, 'original_file was not found')
-    end
-  end
-
-  context "when the file set's work is in a collection" do
-    let(:work)       { build(:monograph) }
-    let(:collection) { build(:collection) }
-
-    before do
-      allow(file_set).to receive(:parent).and_return(work)
-      allow(work).to receive(:in_collections).and_return([collection])
-      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-      allow(file).to receive(:save!)
-      allow(file_set).to receive(:update_index)
-      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-    end
-
-    it "reindexes the collection" do
-      expect(collection).to receive(:update_index)
-      described_class.perform_now(file_set, file.id)
-    end
-  end
-
-  context "when there's a preexisting IIIF cached file" do
-    include FakeFS::SpecHelpers
-    let(:cached_file) { Rails.root.join('tmp', 'network_files', Digest::MD5.hexdigest(file_set.original_file.uri.to_s)) }
-
-    it "deletes the cached file" do
-      FileUtils.mkdir_p Rails.root.join('tmp', 'network_files')
-      FileUtils.touch cached_file
-      expect(cached_file.exist?).to be true
-
-      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-      allow(file).to receive(:save!)
-      allow(file_set).to receive(:update_index)
-      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-      described_class.perform_now(file_set, file.id)
-
-      expect(cached_file.exist?).to be false
-    end
-  end
-
-  context "featured representative" do
-    let(:file_path) { Rails.root + 'tmp' + 'uploads' + 'ab' + 'c1' + '23' + '45' + 'abc12345' + file_type }
-    let(:file) do
-      Hydra::PCDM::File.new.tap do |f|
-        f.content = 'foo'
-        f.original_name = file_type
-        f.save!
-      end
-    end
-    let(:monograph) { build(:monograph, id: 'mono_id', press: 'press') }
-
-    before do
-      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-      allow(file).to receive(:save!)
-      allow(file_set).to receive(:update_index)
-      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-      allow(file_set).to receive(:parent).and_return(monograph)
-    end
-
-    after { FeaturedRepresentative.destroy_all }
-
-    FeaturedRepresentative::KINDS.each do |kind|
-      context kind.to_s do
-        let(:file_type) { "file.#{kind}" }
-
-        before { allow(UnpackJob).to receive(:perform_later).and_return(true) }
-
-        it "unpacks some kinds" do
-          create(:featured_representative, work_id: monograph.id, file_set_id: file_set.id, kind: kind)
-          described_class.perform_now(file_set, file.id)
-          case kind
-          when 'epub', 'webgl', 'pdf_ebook'
-            expect(UnpackJob).to have_received(:perform_later).with(file_set.id, kind)
-          else
-            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, kind)
-          end
-        end
-      end
-    end
-  end
-
-  context 'resource type' do
-    let(:file_path) { Rails.root + 'tmp' + 'uploads' + 'ab' + 'c1' + '23' + '45' + 'abc12345' + file_type }
-    let(:file_type) { 'file.zip' }
-    let(:file) do
-      Hydra::PCDM::File.new.tap do |f|
-        f.content = 'foo'
-        f.original_name = file_type
-        f.save!
-      end
-    end
-    let(:monograph) { build(:monograph, id: 'mono_id', press: 'press') }
-
-    before do
-      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
-      allow(file).to receive(:save!)
-      allow(file_set).to receive(:update_index)
-      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
-      allow(file_set).to receive(:parent).and_return(monograph)
-    end
-
-    %w[interactive\ map map foo].each do |resource_type|
-      context resource_type do
-        let(:resource_type) { resource_type }
-
-        before { allow(UnpackJob).to receive(:perform_later).and_return(true) }
-
-        it "unpacks some resource types" do
-          described_class.perform_now(file_set, file.id)
-          case resource_type
-          when 'interactive_map'
-            expect(UnpackJob).to have_received(:perform_later).with(file_set.id, resource_type)
-          when 'map'
-            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, resource_type)
-          else
-            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, resource_type)
-          end
-        end
-      end
+      expect { described_class.perform_now(file_set, file.id) }.to raise_error(StandardError, /original_file was not found/)
     end
   end
 
@@ -216,8 +105,8 @@ describe CharacterizeJob do
           expect(file_set.label).to eq 'new_filename.jpg'
         end
 
-        # see https://tools.lib.umich.edu/jira/browse/HELIO-4226 (and Hyrax #5670 and #5671)
-        context 'original_name, which is ASCII-8BIT, contains non-ASCII characters' do
+        # https://github.com/samvera/hyrax/issues/5671
+        context 'original_name, which has encoding set to ASCII-8BIT, contains non-ASCII characters' do
           before do
             allow(file_set).to receive_message_chain(:characterization_proxy, :original_name)
                                    .and_return(String.new('ファイル.txt', encoding: 'ASCII-8BIT')) # rubocop:disable RSpec/MessageChain
@@ -237,6 +126,7 @@ describe CharacterizeJob do
       context 'title and label were not previously the same' do
         let(:title) { ['My User-Entered Title'] }
         let(:label) { 'old_filename.jpg' }
+        let(:original_name) { 'new_filename.jpg' }
 
         before do
           allow(file_set).to receive_message_chain(:characterization_proxy, :original_name)
@@ -284,6 +174,109 @@ describe CharacterizeJob do
           expect(file_set.date_modified).to eq 'old_mod_date'
           described_class.perform_now(file_set, file.id)
           expect(file_set.date_modified).to eq 'new_mod_date'
+        end
+      end
+    end
+  end
+
+  # Heliotrope tests follow
+
+  context "when there's a preexisting IIIF cached file" do
+    include FakeFS::SpecHelpers
+    let(:cached_file) { Rails.root.join('tmp', 'network_files', Digest::MD5.hexdigest(file_set.original_file.uri.to_s)) }
+
+    it "deletes the cached file" do
+      FileUtils.mkdir_p Rails.root.join('tmp', 'network_files')
+      FileUtils.touch cached_file
+      expect(cached_file.exist?).to be true
+
+      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, filename)
+      allow(file).to receive(:save!)
+      allow(file_set).to receive(:update_index)
+      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, filename)
+      described_class.perform_now(file_set, file.id)
+
+      expect(cached_file.exist?).to be false
+    end
+  end
+
+  context "featured representative" do
+    let(:file_path) { (Rails.root + 'tmp' + 'uploads' + 'ab' + 'c1' + '23' + '45' + 'abc12345' + file_type).to_s }
+    let(:file) do
+      Hydra::PCDM::File.new.tap do |f|
+        f.content = 'foo'
+        f.original_name = file_type
+        f.save!
+      end
+    end
+    let(:monograph) { build(:monograph, id: 'mono_id', press: 'press') }
+
+    before do
+      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, file_path)
+      allow(file).to receive(:save!)
+      allow(file_set).to receive(:update_index)
+      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, file_path)
+      allow(file_set).to receive(:parent).and_return(monograph)
+    end
+
+    after { FeaturedRepresentative.destroy_all }
+
+    FeaturedRepresentative::KINDS.each do |kind|
+      context kind.to_s do
+        let(:file_type) { "file.#{kind}" }
+
+        before { allow(UnpackJob).to receive(:perform_later).and_return(true) }
+
+        it "unpacks some kinds" do
+          create(:featured_representative, work_id: monograph.id, file_set_id: file_set.id, kind: kind)
+          described_class.perform_now(file_set, file.id)
+          case kind
+          when 'epub', 'webgl', 'pdf_ebook'
+            expect(UnpackJob).to have_received(:perform_later).with(file_set.id, kind)
+          else
+            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, kind)
+          end
+        end
+      end
+    end
+  end
+
+  context 'resource type' do
+    let(:file_path) { (Rails.root + 'tmp' + 'uploads' + 'ab' + 'c1' + '23' + '45' + 'abc12345' + file_type).to_s }
+    let(:file_type) { 'file.zip' }
+    let(:file) do
+      Hydra::PCDM::File.new.tap do |f|
+        f.content = 'foo'
+        f.original_name = file_type
+        f.save!
+      end
+    end
+    let(:monograph) { build(:monograph, id: 'mono_id', press: 'press') }
+
+    before do
+      allow(Hydra::Works::CharacterizationService).to receive(:run).with(file, file_path)
+      allow(file).to receive(:save!)
+      allow(file_set).to receive(:update_index)
+      allow(CreateDerivativesJob).to receive(:perform_later).with(file_set, file.id, file_path)
+      allow(file_set).to receive(:parent).and_return(monograph)
+    end
+
+    %w[interactive\ map map foo].each do |resource_type|
+      context resource_type do
+        let(:resource_type) { resource_type }
+
+        before { allow(UnpackJob).to receive(:perform_later).and_return(true) }
+
+        it "unpacks some resource types" do
+          described_class.perform_now(file_set, file.id)
+          case resource_type
+          when 'interactive_map'
+            expect(UnpackJob).to have_received(:perform_later).with(file_set.id, resource_type)
+          when 'map'
+            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, resource_type)
+          else
+            expect(UnpackJob).not_to have_received(:perform_later).with(file_set.id, resource_type)
+          end
         end
       end
     end
