f89d5124dd369d8a040345d97d15c038d903715d
Seth A. Johnson <sethajoh@umich.edu>
2022-11-15 09:41:14 -0500

    HELIO-4361 changes to the PSI report

---
+++ app/jobs/psi_report_job.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+########################################
+## NOTE: THIS IS RUN FROM A CRON JOB! ##
+########################################
+
+# HELIO-4361 run on the first of the month for the previous month
+# Deliver via SFTP
+
+require 'net/ftp'
+
+class PsiReportJob < ApplicationJob
+  def perform(subdomain = "michigan", given_start_date = nil, given_end_date = nil)
+    ActiveRecord::Base.retrieve_connection unless ActiveRecord::Base.connected? # HELIO-3844
+
+    subdomain = subdomain || "michigan"
+    press = Press.where(subdomain: subdomain).first
+
+    report_time = PsiReport::ReportTime.new(given_start_date, given_end_date)
+    csv = build_report(press, report_time)
+    file_path = File.join("/", "tmp", "fulcrum_psi_report_#{report_time.start_date}_#{report_time.end_date}.csv")
+    File.write(file_path, csv)
+
+    config = Rails.root.join('config', 'psi_sftp.yml')
+    if File.exist? config
+      Rails.logger.info("PSI_REPORT: #{config} credentials exist, sending #{file_path} via SFTP")
+      yaml = YAML.safe_load(File.read(config))
+      psi_sftp = yaml['psi_credentials']
+
+      ftp = Net::FTP.open(psi_sftp["ftp"], username: psi_sftp["user"], password: psi_sftp["password"], ssl: true)
+      ftp.put(file_path)
+      ftp.close
+    else
+      Rails.logger.info("PSI_REPORT: #{config} credentials not present, #{file_path} report not sent")
+    end
+  end
+
+  def build_report(press, report_time)
+    counter = CounterReport.requests
+                           .press(press) # will automatically include child presses
+                           .start_date(report_time.start_date.to_datetime)
+                           .end_date(report_time.end_date.to_datetime)
+
+    # There's some weird stuff where the noid and the parent_noid can be the same (the monograph's)
+    # but that currently only happens with Investigations, not Requests, so this should work ok...
+    file_sets = presenters_for(Hyrax::FileSetPresenter, counter.pluck(:noid).uniq)
+    monographs = presenters_for(Hyrax::MonographPresenter, counter.pluck(:parent_noid).uniq)
+
+    header = ["Event Date",
+              "Event",
+              "ISBN/DOI",
+              "Publisher Name",
+              "Book Title/Journal Title",
+              "Author(s)",
+              "Chapter/Article Title",
+              "IP Adress",
+              "OA/Paid",
+              "Journal Imprint",
+              "Orcid ID",
+              "Affiliation",
+              "Funders"]
+
+    csv = CSV.generate({}) do |row|
+      row << header
+
+      counter.each do |c|
+        ip = c.session.split("|")[0]
+        chapter = c.section_type == "Chapter" ? c.section : file_sets[c.noid]&.page_title
+
+        row << [
+          c.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+          "request",
+          which_doi(c, monographs, file_sets),
+          monographs[c.parent_noid]&.publisher.first,
+          monographs[c.parent_noid]&.page_title,
+          creators(monographs[c.parent_noid]),
+          chapter,
+          ip,
+          monographs[c.parent_noid]&.open_access? ? "TRUE" : "FALSE",
+          "", # leave Journal Imprint blank
+          orcids(monographs[c.parent_noid]),
+          "", # Affiliation aka ROR
+          "" # Funder ID
+        ]
+      end
+    end
+
+    csv
+  end
+
+  def which_doi(c, monographs, file_sets)
+    # file_set doi if it exists, otherwise the monograph doi (for epubs and other featured reps), otherwise the monograph isbn(s)
+    return file_sets[c.noid]&.doi_url if  file_sets[c.noid]&.doi?
+    return monographs[c.parent_noid]&.doi_url if monographs[c.parent_noid]&.doi?
+    return monographs[c.parent_noid]&.isbn.join("; ") if monographs[c.parent_noid]&.isbn.present?
+    ""
+  end
+
+  def creators(presenter)
+    presenter.creator.join(";") # no spaces per the example in HELIO-4361
+  end
+
+
+  def orcids(presenter)
+    return "" if presenter.creator_orcids.blank?
+    presenter.creator_orcids.join(";")
+  end
+
+  def presenters_for(hyrax_presenter, noids)
+    presenters = {}
+    until noids.empty?
+      Hyrax::PresenterFactory.build_for(ids: noids.shift(999), presenter_class: hyrax_presenter, presenter_args: nil).map do |p|
+        presenters[p.id] = p
+      end
+    end
+    presenters
+  end
+end
+
+
+module PsiReport
+  class ReportTime
+    attr_reader :given_start_date, :given_end_date
+
+    def initialize(given_start_date = nil, given_end_date = nil)
+      raise "given_start_date must be in format YYYY-MM-DD" if given_start_date.present? && !given_start_date.match?(/\d\d\d\d-\d\d-\d\d/)
+      raise "given_end_date must be in format YYYY-MM-DD" if given_end_date.present? && !given_end_date.match?(/\d\d\d\d-\d\d-\d\d/)
+
+      @given_start_date = Date.parse(given_start_date) if given_start_date.present?
+      @given_end_date = Date.parse(given_end_date) if given_end_date.present?
+    end
+
+    def start_date
+      # First day of last month
+      return @given_start_date.to_s if @given_start_date.present?
+      Time.zone.today.at_beginning_of_month.prev_month.to_s
+    end
+
+    def end_date
+      # Last day of last month
+      return @given_end_date.to_s if @given_end_date.present?
+      (Time.zone.today - Time.zone.today.mday).to_s
+    end
+  end
+end
--- lib/tasks/usage_reports/psi_report.rake
+++ lib/tasks/usage_reports/psi_report.rake
@@ -3,60 +3,19 @@
 # See FULCRUMOPS-13
 # press will include subpresses
 # Updated with HELIO-4121
+# Updated with HELIO-4361
 desc "Create a usage report to send to PSI"
 namespace :heliotrope do
-  task :psi_report, [:press] => :environment do |_t, args|
-    # Usage: bundle exec rake "heliotrope:psi_report[michigan]" > /tmp/psi-report.csv
+  task :psi_report, [:press, :start_date, :end_date] => :environment do |_t, args|
+    # Usage: bundle exec rake "heliotrope:psi_report[michigan]"
+    # OR
+    # Usage: bundle exec rake "heliotrope:psi_report[michigan, 2022-01-01, 2022-01-30]"
 
-    fail "You must include a press subdomain" unless args.press.present?
-    press = Press.where(subdomain: args.press).first
-    fail "No Press found for subdomain: #{args.press}" unless press.present?
-    presses = press.children.pluck(:id) << press.id
+    # start_date and end_date are optional
+    # Without them, the report will run for the previous month
+    # The report is placed in /tmp and delivered via sftp
+    # See the PsiReportJob for details.
 
-    counter = CounterReport.where(request: 1, press: presses).where("created_at > ?", 6.months.ago)
-
-    # There's some weird stuff where the noid and the parent_noid can be the same (the monograph's)
-    # but that currently only happens with Investigations, not Requests, so this should work ok...
-    file_sets = presenters_for(Hyrax::FileSetPresenter, counter.pluck(:noid).uniq)
-    monographs = presenters_for(Hyrax::MonographPresenter, counter.pluck(:parent_noid).uniq)
-
-    # puts ["Time Stamp", "IP Address", "Monograph Title", "Chapter/Resource Title", "Creators"].to_csv
-    puts ["Event Date", "Event", "ISBN/DOI", "Publisher Name", "Book Title/Journal Title", "Author(s)", "Chapter/Article Title", "IP Adress", "OA/Paid"].to_csv
-
-    counter.each do |c|
-      ip = c.session.split("|")[0]
-      chapter = c.section_type == "Chapter" ? c.section : file_sets[c.noid]&.title 
-
-      puts [
-            c.created_at, 
-            "request",
-            which_doi(c, monographs, file_sets),
-            monographs[c.parent_noid]&.publisher.first,
-            monographs[c.parent_noid]&.title,
-            monographs[c.parent_noid]&.creator.join("; "),
-            chapter,
-            ip,
-            monographs[c.parent_noid]&.open_access? ? "OA" : "Paid"
-      ].to_csv
-    end
-
-  end
-
-  def which_doi(c, monographs, file_sets)
-    # file_set doi if it exists, otherwise the monograph doi (for epubs and other featured reps), otherwise the monograph isbn(s)
-    return file_sets[c.noid]&.doi_url if  file_sets[c.noid]&.doi?
-    return monographs[c.parent_noid]&.doi_url if monographs[c.parent_noid]&.doi?
-    return monographs[c.parent_noid]&.isbn.join("; ") if monographs[c.parent_noid]&.isbn.present?
-    ""
-  end
-
-  def presenters_for(hyrax_presenter, noids)
-    presenters = {}
-    until noids.empty?
-      Hyrax::PresenterFactory.build_for(ids: noids.shift(999), presenter_class: hyrax_presenter, presenter_args: nil).map do |p|
-        presenters[p.id] = p
-      end
-    end
-    presenters
+    PsiReportJob.perform_later(args.press, args.start_date, args.end_date)
   end
 end
