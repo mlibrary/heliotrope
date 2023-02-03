ec68960e0ed169b6697c9311c796eda7250b4ca6
conorom <conorom@umich.edu>
2023-02-03 11:52:13 -0500

    automatically set date_published on save

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -97,4 +97,187 @@ describe FileSet do
       end
     end
   end
+
+  describe '#maybe_set_date_published' do
+    subject { file_set.date_published }
+
+    let(:file_set) { create(:file_set, visibility: visibility, date_published: date_published) }
+    let(:visibility) { 'restricted' }
+    let(:date_published) { [] }
+    let(:now) { Hyrax::TimeService.time_in_utc }
+    let(:a_week_ago) { Hyrax::TimeService.time_in_utc - 7.days }
+    let(:two_weeks_ago) { Hyrax::TimeService.time_in_utc - 14.days }
+    let(:three_weeks_ago) { Hyrax::TimeService.time_in_utc - 21.days }
+
+    before do
+      allow(Hyrax::TimeService).to receive(:time_in_utc).and_return(now)
+    end
+
+    context 'new FileSets' do
+      context 'no specified date_published value' do
+        context "with 'restricted' visibility" do
+          it 'does not automatically set date_published' do
+            file_set.save
+            expect(subject).to eq []
+          end
+        end
+
+        context "with 'open' visibility" do
+          let(:visibility) { 'open' }
+
+          it 'automatically sets date_published' do
+            file_set.save
+            expect(subject).to eq [now]
+          end
+        end
+      end
+
+      context 'date_published value specified' do
+        let(:date_published) { [three_weeks_ago] }
+
+        context "with 'restricted' visibility" do
+          it 'does not automatically set date_published' do
+            file_set.save
+            expect(subject).to eq [three_weeks_ago]
+          end
+        end
+
+        context "with 'open' visibility" do
+          let(:visibility) { 'open' }
+
+          it 'does not automatically set date_published to `now`' do
+            file_set.save
+            expect(subject).to eq [three_weeks_ago]
+          end
+        end
+      end
+    end
+
+    context 'existing FileSets' do
+      context 'date_published is already set' do
+        let(:date_published) { [three_weeks_ago] }
+
+        context 'visibility is not changing' do
+          it 'does not automatically set date_published' do
+            file_set.title = ['Something New']
+            file_set.save
+            expect(subject).to eq [three_weeks_ago]
+          end
+
+          it 'does not prevent date_published from being set' do
+            file_set.title = ['Something New']
+            file_set.date_published = [a_week_ago]
+            file_set.save
+            expect(subject).to eq [a_week_ago]
+          end
+        end
+
+        context "visibility is changing from 'open' to 'restricted'" do
+          # aside: as these FileSets are being created with 'open' visibility, they would have their date_published...
+          # values immediately set to `now`, as in the example atop, were they not already set to 'three_weeks_ago'.
+          let(:visibility) { 'open' }
+
+          context 'no incoming date_published value' do
+            it 'does not set date_published to `now`' do
+              file_set.visibility = 'restricted'
+              file_set.save
+              expect(subject).to eq [three_weeks_ago]
+            end
+          end
+
+          context 'incoming date_published value' do
+            it 'does not set date_published to `now`, uses incoming value' do
+              file_set.visibility = 'restricted'
+              file_set.date_published = [a_week_ago]
+              file_set.save
+              expect(subject).to eq [a_week_ago]
+            end
+          end
+        end
+
+        context "visibility is changing from 'restricted' to 'open'" do
+          context 'no incoming date_published value' do
+            it 'does not set date_published as it is already populated' do
+              file_set.visibility = 'open'
+              file_set.save
+              expect(subject).to eq [three_weeks_ago]
+            end
+          end
+
+          context 'incoming date_published value' do
+            it 'does not set date_published to `now`, uses incoming value' do
+              file_set.visibility = 'open'
+              file_set.date_published = [a_week_ago]
+              file_set.save
+              expect(subject).to eq [a_week_ago]
+            end
+          end
+        end
+      end
+
+      context 'date_published is not yet set' do
+        context 'visibility is not changing' do
+          context 'no incoming date_published value' do
+            it 'does not automatically set date_published' do
+              file_set.title = ['Something New']
+              file_set.save
+              expect(subject).to eq []
+            end
+          end
+
+          context 'incoming date_published value' do
+            it 'does not set date_published to `now`, uses incoming value' do
+              file_set.title = ['Something New']
+              file_set.date_published = [a_week_ago]
+              file_set.save
+              expect(subject).to eq [a_week_ago]
+            end
+          end
+        end
+
+        context "visibility is changing from 'open' to 'restricted'" do
+          # aside: as these FileSets are being created with 'open' visibility, they would have their date_published...
+          # values set to `now` straight away, as in the example above, were we to not set them to 'two_weeks_ago' here.
+          let(:visibility) { 'open' }
+          let(:date_published) { [two_weeks_ago] }
+
+          context 'no incoming date_published value' do
+            it 'does not set date_published to `now`' do
+              file_set.visibility = 'restricted'
+              file_set.save
+              expect(subject).to eq [two_weeks_ago]
+            end
+          end
+
+          context 'there is an incoming date_published value' do
+            it 'does not set date_published to `now`, uses incoming value' do
+              file_set.visibility = 'restricted'
+              file_set.date_published = [a_week_ago]
+              file_set.save
+              expect(subject).to eq [a_week_ago]
+            end
+          end
+        end
+
+        context "visibility is changing from 'restricted' to 'open'" do
+          context 'no incoming date_published value' do
+            it 'sets date_published to `now`' do
+              file_set.visibility = 'open'
+              file_set.save
+              expect(subject).to eq [now]
+            end
+          end
+
+          context 'there is an incoming date_published value' do
+            it 'does not set date_published to `now`, uses incoming value' do
+              file_set.visibility = 'open'
+              file_set.date_published = [a_week_ago]
+              file_set.save
+              expect(subject).to eq [a_week_ago]
+            end
+          end
+        end
+      end
+    end
+  end
 end
