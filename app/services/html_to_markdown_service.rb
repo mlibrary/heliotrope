54ea71ac6052ceb6d5fba0e37ecbff06c228612d
conorom <conorom@umich.edu>
2023-08-16 13:31:01 -0400

    don't convert TMM HTML to Markdown
    ...i.e. HTML _is_ allowed in certain Fulcrum metadata fields :-)

--- app/services/html_to_markdown_service.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class HtmlToMarkdownService
-  def self.convert(html)
-    # This is the closest "opposite" conversion I can find to Redcarpet. There are slight whitespace differences.
-    ReverseMarkdown.convert(html, github_flavored: true, unknown_tags: :bypass).strip
-  end
-end
