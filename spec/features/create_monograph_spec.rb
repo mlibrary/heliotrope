51a56957422dc5f3e8bafe4d34cf82f9b6734421
jmcglone <jmcglone@umich.edu>
2023-08-17 17:31:09 -0400

    HELIO-4496 - adjusting spec again

--- spec/features/create_monograph_spec.rb
+++ spec/features/create_monograph_spec.rb
@@ -163,7 +163,7 @@ describe 'Create a monograph' do
       # back on Monograph catalog page
       expect(page).to have_content 'Monograph content warning text'
       # the content warning information is shown only when this JS-triggering link is clicked, hence `visible:false`
-      expect(page).to have_link('Expand to read full warning statement...', href: nil)
+      expect(page).to have_link('Expand to read full warning statement...', href: '#content-warning-information')
       expect(page).to have_css("div#content-warning-information", visible: false, text: "Monograph's specific content warning information text, with a link.")
       expect(page).to have_link('link', href: 'https://www.google.com', visible: false)
 
