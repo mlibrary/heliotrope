6893d46c13f19209dd6821bcb8a425c2d3ea1205
conorom <conorom@umich.edu>
2023-09-21 22:29:46 -0400

    rebasing completed with single commit hyrax; restore explain partials rails 6 work

--- config/initializers/explain_partials.rb
+++ config/initializers/explain_partials.rb
@@ -4,23 +4,16 @@
 if Rails.env.development? && ENV['EXPLAIN_PARTIALS']
   module ExplainPartials
     def initialize(body, layout, template)
-      # we really only want this to run for HTML templates, i.e. `ActionView::Template::HTML`
-      # `render :plain` stuff like RIIIF's JSON endpoint is `ActionView::Template::Text` and won't have the necessary...
-      # methods used below to identify its path. Also the added HTML comments will break JSON parsing anyways.
-      @body = if template.class.method_defined?(:short_identifier)
-                if template.class.method_defined?(:locals) && template.locals.present?
-                  "\n<!-- START PARTIAL #{template.short_identifier} locals=#{template.locals.inspect} -->\n" \
-                  "#{body}\n" \
-                  "<!-- END PARTIAL #{template.short_identifier} locals=#{template.locals.inspect} -->\n".html_safe # rubocop:disable Rails/OutputSafety
-                else
-                  "\n<!-- START PARTIAL #{template.short_identifier} -->\n" \
-                  "#{body}\n" \
-                  "<!-- END PARTIAL #{template.short_identifier} -->\n".html_safe # rubocop:disable Rails/OutputSafety
-                end
+      @body = if template.locals.present?
+                "\n<!-- START PARTIAL #{template.short_identifier} locals=#{template.locals.inspect} -->\n" \
+                "#{body}\n" \
+                "<!-- END PARTIAL #{template.short_identifier} locals=#{template.locals.inspect} -->\n".html_safe # rubocop:disable Rails/OutputSafety
               else
-                # do nothing here, it's `render :plain` and probably JSON or something!
-                body
+                "\n<!-- START PARTIAL #{template.short_identifier} -->\n" \
+                "#{body}\n" \
+                "<!-- END PARTIAL #{template.short_identifier} -->\n".html_safe # rubocop:disable Rails/OutputSafety
               end
+
       @layout = layout
       @template = template
     end
