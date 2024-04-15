97f00a1cacdd7656c9e70ab7267f4259e0068f75
Seth A. Johnson <sethajoh@umich.edu>
2024-04-15 15:29:05 -0400

    HELIO-4636 fix blacklight deprecation messages, keep a11y work   * We took on some components/blacklight/response/view_type* because     of a couple of a11y changes we need like role and aria-selected

---
+++ app/components/blacklight/response/view_type_component.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Blacklight
+  module Response
+    # Render spellcheck results for a search query
+    class ViewTypeComponent < ViewComponent::Base
+      renders_many :views, 'Blacklight::Response::ViewTypeButtonComponent'
+
+      # @param [Blacklight::Response] response
+      def initialize(response:, views: {}, search_state:, selected: nil)
+        @response = response
+        @views = views
+        @search_state = search_state
+        @selected = selected
+      end
+
+      def before_render
+        return if views.any?
+
+        @views.each do |key, config|
+          with_view(key: key, view: config, selected: @selected == key, search_state: @search_state)
+        end
+      end
+
+      def render?
+        Deprecation.silence(Blacklight::ConfigurationHelperBehavior) do
+          helpers.has_alternative_views?
+        end
+      end
+    end
+  end
+end
