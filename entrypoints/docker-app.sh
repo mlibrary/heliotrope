e32c183e3c250bf39dc7b97a9eac6ad8665c3a85
Seth A. Johnson <sethajoh@umich.edu>
2024-06-17 09:15:17 -0400

    Update Dockerfile and docker compose

---
+++ entrypoints/docker-app.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mysql -u root -phelio-admin -e "GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'helio' WITH GRANT OPTION;"
+bin/setup
+bundle exec rails checkpoint:migrate
+bundle exec rails system_user
