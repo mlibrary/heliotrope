1246733134b978f407ab49bc16702dc87239ebcf
conorom <conorom@umich.edu>
2025-03-06 09:41:48 -0500

    HELIO-484 and HELIO-4856 (#3766)
    * Change Series label to Collection for livedplaces
    
    * add 'Electromagnetics for Engineers' to ee Monograph list

---
+++ spec/features/monograph_catalog_press_specific_stuff_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Press-specific display on the Monograph Catalog' do
+  context 'Series labelling' do
+    let(:monograph_with_series_values) { create(:public_monograph,
+                                                press: press.subdomain,
+                                                series: ['The Cereal Series', 'The Second Series']) }
+
+    context 'regular, no-override press' do
+      let(:press) { create(:press) }
+
+      it 'Uses "Series" as the label' do
+        visit hyrax_monograph_path(monograph_with_series_values)
+
+        # series - special label for livedplaces press, which uses "Collection" as the label instead of "Series"
+        series_subject_div_text = find('div.subject-series').text
+        expect(series_subject_div_text).to start_with('Series')
+        expect(series_subject_div_text).to_not start_with('Collection')
+        expect(page).to have_content 'The Cereal Series'
+        expect(page).to have_content 'The Second Series'
+      end
+    end
+
+    context 'livedplaces press' do
+      let(:press) { create(:press, subdomain: 'livedplaces') }
+
+      it 'Uses "Collection" as the label' do
+        visit hyrax_monograph_path(monograph_with_series_values)
+
+        # series - special label for livedplaces press, which uses "Collection" as the label instead of "Series"
+        series_subject_div_text = find('div.subject-series').text
+        expect(series_subject_div_text).to start_with('Collection')
+        expect(series_subject_div_text).to_not start_with('Series')
+        expect(page).to have_content 'The Cereal Series'
+        expect(page).to have_content 'The Second Series'
+      end
+    end
+  end
+end
