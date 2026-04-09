14daede66e6b9528284bebab1c0504da13c767a6
Seth A. Johnson <sethajoh@umich.edu>
2026-04-09 16:50:44 -0400

    HELIO-5053 Upgrade from webpacker to shakapacker

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
-const environment = require('./environment')
+const webpackConfig = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = webpackConfig
