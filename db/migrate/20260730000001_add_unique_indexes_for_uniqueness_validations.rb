bf7c9a76f4c537e8ab17cd6832e0518a5b7d8625
conorom <conorom@umich.edu>
2026-08-04 15:09:17 -0400

    migration duplicates existing keys

--- db/migrate/20260730000001_add_unique_indexes_for_uniqueness_validations.rb
+++ db/migrate/20260730000001_add_unique_indexes_for_uniqueness_validations.rb
@@ -4,11 +4,6 @@ class AddUniqueIndexesForUniquenessValidations < ActiveRecord::Migration[6.1]
   def change
     add_index :ebook_table_of_contents_caches, :noid, unique: true
 
-    change_table :featured_representatives, bulk: true do |t|
-      t.index :file_set_id, unique: true
-      t.index %i[work_id kind], unique: true
-    end
-
     change_table :presses, bulk: true do |t|
       t.index :name, unique: true
       t.index :subdomain, unique: true
