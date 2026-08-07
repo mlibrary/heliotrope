365bf742f98ecf58506fe8692236702ac932942a
Seth A. Johnson <sethajoh@umich.edu>
2026-08-07 14:40:30 -0400

    HELIO-5100 get resque_web working with twitter-bootstrap-rails 5.3 This involves aggressive overrides of the resque_web gem. I don't expact that gem will ever be updated, last update was 9 years ago, so if we're going to use it we're going to need to take ownership of big parts of it.

---
+++ app/assets/javascripts/resque_web/application.js
@@ -0,0 +1,8 @@
+// Resque-web override: avoid twitter-bootstrap-rails Bootstrap 5 minified JS,
+// which Uglifier cannot parse in our Sprockets/Uglifier stack.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require_tree .
+
